cb3a96aab4ad0323c14e90b098a28593a37497b4
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-06-20 09:04:14 +0900

    [level 2] Title: 숫자의 표현, Time: 1.40 ms, Memory: 58.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+
+            for (int j = i; j <= n; j++) {
+                
+                sum += j;
+
+                if (sum > n) {
+                    break;
+                }
+
+                if (sum == n) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
