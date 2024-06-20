5fdc5fd78b4a2cd239af69a61308eb296821626a
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-06-21 08:41:39 +0900

    [Gold II] Title: 친구 네트워크, Time: 1036 ms, Memory: 71348 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -1,66 +1,83 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
-
+ 
 public class Main {
-
-    static HashMap<String, String> friend;
-    static HashMap<String, Integer> size;
-
-    public static void main(String[] args) throws IOException {
-
+    static int[] parent; // 유니온 파인드 루트 노드 배열
+    static int[] level; // 각 노드마다 층의 개수
+ 
+    public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-
-            friend = new HashMap<>();
-            size = new HashMap<>();
-
-            int f = Integer.parseInt(br.readLine());
-
-            for (int i = 0; i < f; i++) {
-                StringTokenizer st = new StringTokenizer(br.readLine());
-
-                String f1 = st.nextToken();
-                String f2 = st.nextToken();
-
-                if (!friend.containsKey(f1)) {
-                    friend.put(f1, f1);
-                    size.put(f1, 1);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+ 
+        int T = Integer.parseInt(br.readLine());
+ 
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            int F = Integer.parseInt(br.readLine());
+ 
+            parent = new int[F * 2];
+            level = new int[F * 2];
+            for (int i = 0; i < F * 2; i++) {
+                parent[i] = i;
+                level[i] = 1;
+            }
+ 
+            int idx = 0;
+            Map<String, Integer> map = new HashMap<>();
+ 
+            for (int i = 0; i < F; i++) {
+                st = new StringTokenizer(br.readLine());
+                String a = st.nextToken();
+                String b = st.nextToken();
+ 
+                if (!map.containsKey(a)) {
+                    map.put(a, idx++);
                 }
-
-                if (!friend.containsKey(f2)) {
-                    friend.put(f2, f2);
-                    size.put(f2, 1);
+ 
+                if (!map.containsKey(b)) {
+                    map.put(b, idx++);
                 }
-
-                union(f1, f2);
-                System.out.println(size.get(find(f1)));
+ 
+                sb.append(union(map.get(a), map.get(b)) + "\n");
             }
         }
+ 
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
-
-    private static void union(String f1, String f2) {
-        f1 = find(f1);
-        f2 = find(f2);
-
-        if (!f1.equals(f2)) {
-            friend.put(f1, f2);
-            size.put(f2, size.get(f1) + size.get(f2));
+ 
+    public static int find(int x) {
+        if (x == parent[x]) {
+            return x;
         }
+ 
+        return parent[x] = find(parent[x]);
     }
-
-    private static String find(String f) {
-        if (f.equals(friend.get(f))) {
-            return f;
+ 
+    // 합집합 연산
+    public static int union(int x, int y) {
+        x = find(x);
+        y = find(y);
+ 
+        // 항상 x < y인 값이 들어온다고 가정
+        if (x != y) {
+            parent[y] = x;
+            level[x] += level[y]; // y에 있던 층의 개수를 더해 줌.
+ 
+            // 어차피 y의 부모는 항상 x이므로 level[y]를 접근할 일은 없으므로 꼭 이렇게 초기화해 줄 필요는 없음.
+            level[y] = 1;
         }
-
-        String root = find(friend.get(f));
-        friend.put(f, root);
-
-        return root;
+ 
+        return level[x];
     }
+ 
 }
