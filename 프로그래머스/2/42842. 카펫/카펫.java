3bf2b08e43c94be71f207b93ae65b704d3794d65
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-06-23 13:02:19 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int total = brown + yellow; //전체 넓이
+        int half = (brown + 4) / 2;
 
-        for (int height = 3; height <= Math.sqrt(total); height++) { //높이(세로)
-            if (total % height == 0) {
-                int width = total / height; //너비(가로)
+        for (int height = 3; height < half; height++) {
+            int width = half - height;
 
-                if ((height - 2) * (width - 2) == yellow) { //노란색 격자의 수 조건 확인
-                    return new int[]{width, height};
-                }
+            if (width * height == brown + yellow) {
+                return new int[]{width, height};
             }
         }
 
