2b8bcab13bce76fbbe9d8a58f22c00a0a2088a45
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-06-25 20:16:22 +0900

    [Gold II] Title: 퍼즐, Time: 860 ms, Memory: 77688 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -2,118 +2,81 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static class Puzzle {
-        int[][] state;
-        int x, y, count;
-
-        public Puzzle(int[][] state, int x, int y, int count) {
-            this.state = state;
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
-    }
-
-    static int[][] goal = {{1, 2, 3}, {4, 5, 6}, {7, 8, 0}};
+    static String goal = "123456780";
+    static HashMap<String, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[][] init = new int[3][3];
-        int x = 0;
-        int y = 0;
+
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < 3; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                init[i][j] = Integer.parseInt(st.nextToken());
-                if (init[i][j] == 0) {
-                    x = i;
-                    y = j;
-                }
+                sb.append(st.nextToken());
             }
         }
 
-        System.out.println(bfs(new Puzzle(init, x, y, 0)));
+        String init = sb.toString();
+        map.put(init, 0);
+
+        System.out.println(bfs(init));
     }
 
-    private static int bfs(Puzzle init) {
-
-        HashSet<String> visit = new HashSet<>();
-        Queue<Puzzle> qu = new ArrayDeque<>();
-
-        qu.offer(init);
-        visit.add(arrayToString(init.state));
+    private static int bfs(String init) {
 
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
 
-        while (!qu.isEmpty()) {
-            Puzzle now = qu.poll();
+        Queue<String> qu = new ArrayDeque<>();
+        qu.offer(init);
 
-            if (check(now.state)) {
-                return now.count;
+        while (!qu.isEmpty()) {
+            String now = qu.poll();
+
+            int count = map.get(now);
+
+            if (now.equals(goal)) {
+                return count;
             }
 
+            int zero = now.indexOf("0");
+            int x = zero / 3;
+            int y = zero % 3;
 
             for (int i = 0; i < 4; i++) {
-                int nx = now.x + dx[i];
-                int ny = now.y + dy[i];
-
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
                 if (nx >= 0 && nx < 3 && ny >= 0 && ny < 3) {
-                    int[][] copy = getCopy(now.state);
-                    copy[now.x][now.y] = copy[nx][ny];
-                    copy[nx][ny] = 0;
 
-                    String target = arrayToString(copy);
-                    if (!visit.contains(target)) {
-                        visit.add(target);
-                        qu.offer(new Puzzle(copy, nx, ny, now.count + 1));
+                    int nextZero = nx * 3 + ny;
+
+                    String next = swap(now, zero, nextZero);
+
+                    if (!map.containsKey(next)) {
+                        map.put(next, count + 1);
+                        qu.offer(next);
                     }
                 }
             }
         }
-        
+
         return -1;
     }
 
-    private static int[][] getCopy(int[][] arr) {
-
-        int[][] copy = new int[3][3];
-
-        for (int i = 0; i < 3; i++) {
-            System.arraycopy(arr[i], 0, copy[i], 0, 3);
-        }
-
-        return copy;
-    }
-
-    private static boolean check(int[][] now) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (goal[i][j] != now[i][j]) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private static String arrayToString(int[][] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int[] ints : arr) {
-            for (int i : ints) {
-                sb.append(i);
-            }
-        }
-        return sb.toString();
+    private static String swap(String s, int i, int j) {
+        char[] charArray = s.toCharArray();
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return new String(charArray);
     }
 }
