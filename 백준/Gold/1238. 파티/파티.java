6a5c27ed593ce37bb2e3a4a506edc0de0d33aba3
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-06-26 08:57:42 +0900

    [Gold III] Title: 파티, Time: 220 ms, Memory: 19436 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -18,17 +19,11 @@ public class Main {
         int x = Integer.parseInt(st.nextToken()); //도착 지점
 
         ArrayList<Node>[] graph = new ArrayList[n + 1];
-
-        int[][] dist = new int[n + 1][n + 1];
-        boolean[][] visit = new boolean[n + 1][n + 1];
+        ArrayList<Node>[] reversedGraph = new ArrayList[n + 1];
 
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
-            for (int j = 1; j <= n; j++) {
-                if (i != j) {
-                    dist[i][j] = Integer.MAX_VALUE;
-                }
-            }
+            reversedGraph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < m; i++) {
@@ -39,41 +34,51 @@ public class Main {
             int v = Integer.parseInt(st.nextToken());
 
             graph[s].add(new Node(e, v));
+            reversedGraph[e].add(new Node(s, v));
         }
 
+        int[] distToX = dijkstra(n, reversedGraph, x);
+        int[] distFromX = dijkstra(n, graph, x);
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i != x) {
+                max = Math.max(max, distToX[i] + distFromX[i]);
+            }
+        }
+
+        System.out.println(max);
+    }
+
+    private static int[] dijkstra(int n, ArrayList<Node>[] graph, int start) {
+
         Queue<Node> qu = new PriorityQueue<>();
+        qu.offer(new Node(start, 0));
 
-        for (int start = 1; start <= n; start++) {
+        int[] dist = new int[n + 1];
+        boolean[] visit = new boolean[n + 1];
 
-            qu.offer(new Node(start, 0));
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
 
-            while (!qu.isEmpty()) {
-                Node now = qu.poll();
+        while (!qu.isEmpty()) {
+            Node now = qu.poll();
 
-                if (visit[start][now.adj]) {
-                    continue;
-                }
-                visit[start][now.adj] = true;
+            if (visit[now.adj]) {
+                continue;
+            }
+            visit[now.adj] = true;
 
-                for (Node next : graph[now.adj]) {
+            for (Node next : graph[now.adj]) {
 
-                    if (dist[start][next.adj] > dist[start][now.adj] + next.v) {
-
-                        dist[start][next.adj] = dist[start][now.adj] + next.v;
-
-                        qu.offer(new Node(next.adj, dist[start][next.adj]));
-                    }
+                if (dist[next.adj] > dist[now.adj] + next.v) {
+                    dist[next.adj] = dist[now.adj] + next.v;
+                    qu.offer(new Node(next.adj, dist[next.adj]));
                 }
             }
         }
 
-        int max = 0;
-        for (int i = 1; i <= n; i++) {
-            max = Math.max(max, dist[i][x] + dist[x][i]);
-        }
-
-        System.out.println(max);
-
+        return dist;
     }
 
     static class Node implements Comparable<Node> {
@@ -83,7 +88,6 @@ public class Main {
             this.adj = adj;
             this.v = v;
         }
-
 
         @Override
         public int compareTo(Node o) {
