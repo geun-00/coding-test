c388bc70d62ebdc74e0a252a5d123e9f54622743
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-06-26 09:43:25 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 16228 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[100_001];
+        Arrays.fill(arr, Integer.MAX_VALUE);
+        arr[n] = 0;
+
+        Queue<Integer> qu = new ArrayDeque<>();
+        qu.offer(n);
+
+        while (!qu.isEmpty()) {
+            int now = qu.poll();
+
+            if (now == k) {
+                System.out.println(arr[now]);
+                return;
+            }
+
+            if (now * 2 <= 100_000 && arr[now * 2] > arr[now]) {
+                arr[now * 2] = arr[now];
+                qu.offer(now * 2);
+            }
+            if (now + 1 <= 100_000 && arr[now + 1] > arr[now] + 1) {
+                arr[now + 1] = arr[now] + 1;
+                qu.offer(now + 1);
+            }
+            if (now - 1 >= 0 && arr[now - 1] > arr[now] + 1) {
+                arr[now - 1] = arr[now] + 1;
+                qu.offer(now - 1);
+            }
+        }
+    }
+}
