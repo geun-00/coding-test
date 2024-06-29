24fde8cf51de00d59dea45983a68a78b6f437064
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-06-29 16:04:54 +0900

    [level 2] Title: 괄호 회전하기, Time: 9.00 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+       int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (check(s)) {
+                count++;
+            }
+
+            s = s.substring(1) + s.charAt(0);
+        }
+        return count;
+    }
+    
+     public boolean check(String s) {
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char now = s.charAt(i);
+
+            if (now == '(' || now == '[' || now == '{') {
+                stack.push(now);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (now == ')' && stack.peek() == '(') {
+                    stack.pop();
+                }
+                else if (now == ']' && stack.peek() == '[') {
+                    stack.pop();
+                }
+                else if (now == '}' && stack.peek() == '{') {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.isEmpty();
+     }
+}
