2701b9ce112644da8c80d26f10c5bfaf44501e4c
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-07-05 09:47:02 +0900

    [Gold IV] Title: 알고스팟, Time: 160 ms, Memory: 15296 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,18 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
 
     static int[][] map;
     static int n, m;
-
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int min = Integer.MAX_VALUE;
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,51 +33,45 @@ public class Main {
     }
 
     private static void bfs() {
-
-        Queue<Point> qu = new PriorityQueue<>();
-        boolean[][] visit = new boolean[n][m];
-
-        qu.offer(new Point(0, 0, 0));
-        visit[0][0] = true;
-
-        while (!qu.isEmpty()) {
-            Point now = qu.poll();
-
-            if (now.x == n - 1 && now.y == m - 1) {
-                System.out.println(now.count);
-                return;
+        Deque<Point> deque = new ArrayDeque<>();
+        int[][] dist = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dist[i][j] = Integer.MAX_VALUE;
             }
+        }
+
+        deque.offerFirst(new Point(0, 0));
+        dist[0][0] = 0;
+
+        while (!deque.isEmpty()) {
+            Point now = deque.pollFirst();
 
             for (int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
 
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visit[nx][ny]) {
-                    if (map[nx][ny] == 0) {
-                        qu.offer(new Point(nx, ny, now.count));
-                    } else {
-                        qu.offer(new Point(nx, ny, now.count + 1));
-                        map[nx][ny] = 0;
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    if (map[nx][ny] == 0 && dist[nx][ny] > dist[now.x][now.y]) {
+                        dist[nx][ny] = dist[now.x][now.y];
+                        deque.offerFirst(new Point(nx, ny));
+                    } else if (map[nx][ny] == 1 && dist[nx][ny] > dist[now.x][now.y] + 1) {
+                        dist[nx][ny] = dist[now.x][now.y] + 1;
+                        deque.offerLast(new Point(nx, ny));
                     }
-
-                    visit[nx][ny] = true;
                 }
             }
         }
+
+        System.out.println(dist[n-1][m-1]);
     }
 
-    static class Point implements Comparable<Point> {
-        int x, y, count;
+    static class Point {
+        int x, y;
 
-        public Point(int x, int y, int count) {
+        public Point(int x, int y) {
             this.x = x;
             this.y = y;
-            this.count = count;
-        }
-
-        @Override
-        public int compareTo(Point o) {
-            return this.count - o.count;
         }
     }
 }
