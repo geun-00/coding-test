ad3343510e0f8c7cce36db04c5a9678e94aad1a8
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-07-07 09:11:55 +0900

    [Gold IV] Title: 타일 채우기, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[31];
+
+        dp[0] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 2] * 3;
+
+            for (int j = 4; j <= i; j += 2) {
+                dp[i] += 2 * dp[i - j];
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
