621088e29a34c56a3179668968cd7461897a054e
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-07-09 20:12:52 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -4,6 +4,5 @@ SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, CASE
                                                           ELSE '거래완료'
                                                       END AS STATUS
 FROM USED_GOODS_BOARD
-# WHERE DATE_FORMAT(CREATED_DATE, '%Y-%d-%m') = '2022-10-05'
-WHERE YEAR(CREATED_DATE) = 2022 AND MONTH(CREATED_DATE) = 10 AND DAY(CREATED_DATE) = 5
+WHERE DATE_FORMAT(CREATED_DATE,'%Y-%m-%d') = '2022-10-05'
 ORDER BY BOARD_ID DESC
