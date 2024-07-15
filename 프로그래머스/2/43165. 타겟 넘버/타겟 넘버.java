3cd9b349191239fa098ff22c9570e9dd6974bf38
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-07-15 20:30:27 +0900

    [level 2] Title: 타겟 넘버, Time: 4.72 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,22 @@
+class Solution {
+    
+    int count = 0;
+    
+    public int solution(int[] numbers, int target) {
+        dfs(0, numbers, target, 0);
+
+        return count;
+    }
+    
+    public void dfs(int depth, int[] numbers, int target, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) {
+                count++;
+            }
+            return;
+        }
+
+        dfs(depth + 1, numbers, target, sum + numbers[depth]);
+        dfs(depth + 1, numbers, target, sum - numbers[depth]);
+    }
+}
