a222b33fd3ea4f43b68f657d944fba1aaa5bd9db
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-07-19 09:08:02 +0900

    [Gold IV] Title: 서강그라운드, Time: 136 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -9,7 +9,6 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int[][] dist;
-    static ArrayList<Node>[] graph;
 
     public static void main(String[] args) throws IOException {
 
@@ -22,13 +21,11 @@ public class Main {
 
         int[] items = new int[n + 1];
         dist = new int[n + 1][n + 1];
-        graph = new ArrayList[n + 1];
 
         st = new StringTokenizer(br.readLine());
 
         for (int i = 1; i <= n; i++) {
             items[i] = Integer.parseInt(st.nextToken());
-            graph[i] = new ArrayList<>();
             Arrays.fill(dist[i], 100 * 100 * 15 + 1);
             dist[i][i] = 0;
         }
@@ -40,12 +37,12 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int l = Integer.parseInt(st.nextToken());
 
-            //다익스트라
-            graph[a].add(new Node(b, l));
-            graph[b].add(new Node(a, l));
+            //플로이드워셜
+            dist[a][b] = l;
+            dist[b][a] = l;
         }
 
-        dijkstra(n);
+        floydWarshall(n);
 
         printResult(n, m, items);
     }
@@ -66,43 +63,16 @@ public class Main {
         System.out.println(max);
     }
 
-    private static void dijkstra(int n) {
+    private static void floydWarshall(int n) {
 
-        boolean[][] visit = new boolean[n + 1][n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            PriorityQueue<Node> qu = new PriorityQueue<>();
-            qu.offer(new Node(i, 0));
-
-            while (!qu.isEmpty()) {
-                Node now = qu.poll();
-
-                if (visit[i][now.adj]) {
-                    continue;
-                }
-                visit[i][now.adj] = true;
-
-                for (Node next : graph[now.adj]) {
-                    if (dist[i][next.adj] > dist[i][now.adj] + next.w) {
-                        dist[i][next.adj] = dist[i][now.adj] + next.w;
-                        qu.offer(new Node(next.adj, dist[i][next.adj]));
+        for (int k = 1; k <= n; k++) {
+            for (int s = 1; s <= n; s++) {
+                for (int e = 1; e <= n; e++) {
+                    if (dist[s][e] > dist[s][k] + dist[k][e]) {
+                        dist[s][e] = dist[s][k] + dist[k][e];
                     }
                 }
             }
-        }
-    }
-
-    static class Node implements Comparable<Node> {
-        int adj, w;
-
-        public Node(int adj, int w) {
-            this.adj = adj;
-            this.w = w;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return this.w - o.w;
         }
     }
 }
