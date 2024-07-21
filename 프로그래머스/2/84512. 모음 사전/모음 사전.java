3f1eec3b74392a6cb4776ed7bad1d690561f6491
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-07-21 11:14:11 +0900

    [level 2] Title: 모음 사전, Time: 0.05 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,20 @@
-import java.util.ArrayList;
-
 class Solution {
-    
-    char[] vowel = {'A', 'E', 'I', 'O', 'U'};
-    ArrayList<String> words = new ArrayList<>();
-    
     public int solution(String word) {
+        char[] vowels = {'A', 'E', 'I', 'O', 'U'};
+        int[] weights = {781, 156, 31, 6, 1};
         
-        dfs(0, "");
+        int index = 0;
         
-        return words.indexOf(word);
-    }
-    
-    public void dfs(int depth, String s){
-        
-        words.add(s);
-        
-        if(depth == 5) {
-            return;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            for (int j = 0; j < 5; j++) {
+                if (c == vowels[j]) {
+                    index += weights[i] * j;
+                    break;
+                }
+            }
         }
         
-        for(char c : vowel){
-            dfs(depth + 1, s + c);
-        }
+        return index + word.length();
     }
 }
