b135aecac86504eac6c250346592d7e2549116bc
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-07-25 08:49:30 +0900

    [level 3] Title: 야근 지수, Time: 124.56 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        
+        PriorityQueue<Integer> qu = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int w : works) {
+            qu.offer(w);
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            if (qu.isEmpty() || qu.peek() == 0) {
+                return 0;
+            }
+
+            qu.offer(qu.poll() - 1);
+        }
+
+        long sum = 0;
+
+        while (!qu.isEmpty()) {
+            int num = qu.poll();
+            sum += (long) num * num;
+        }
+
+        return sum;
+    }
+}
