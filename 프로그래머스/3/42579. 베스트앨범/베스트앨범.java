9f5765c9f3a1ae084eb7533b49a7c48aec3aed20
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-07-31 18:15:42 +0900

    [level 3] Title: 베스트앨범, Time: 4.44 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,10 +3,13 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         
-         HashMap<String, Integer> total = new HashMap<>();
+        // 노래 장르별 재생된 횟수의 총합
+        HashMap<String, Integer> total = new HashMap<>();
+        // 각 장르의 노래 고유 번호와 재생 횟수
         HashMap<String, HashMap<Integer, Integer>> songs = new HashMap<>();
 
         for (int i = 0; i < plays.length; i++) {
+            
             total.put(genres[i], total.getOrDefault(genres[i], 0) + plays[i]);
 
             HashMap<Integer, Integer> map = new HashMap<>();
@@ -19,6 +22,7 @@ class Solution {
             }
         }
 
+        // 장르별 재생된 횟수 총합을 기준으로 정렬
         ArrayList<String> list = new ArrayList<>(total.keySet());
         list.sort((o1, o2) -> total.get(o2) - total.get(o1));
 
@@ -27,6 +31,7 @@ class Solution {
         for (String s : list) {
             HashMap<Integer, Integer> map = songs.get(s);
 
+            // 내림차순 정렬한 고유 번호를 추출
             ArrayList<Integer> list1 = new ArrayList<>(map.keySet());
             list1.sort((o1, o2) -> map.get(o2) - map.get(o1));
 
