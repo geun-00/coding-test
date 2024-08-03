fab4c32f37b7dc0634a3d20eed09d3ad26bec22a
장근영 <jky00914@naver.com>
2024-08-03 18:08:19 +0900

    BJ_14890

---
+++ src/Baekjoon/implementation/BJ_14890.java
@@ -0,0 +1,124 @@
+package Baekjoon.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * <a href = "https://www.acmicpc.net/problem/14890">백준 14890번 - 구현 : 경사로</a>
+ * <br>
+ * <a href = "https://velog.io/@jky00914/%EB%B0%B1%EC%A4%80-14890%EB%B2%88-%EA%B2%BD%EC%82%AC%EB%A1%9C">velog</a>
+ *
+ * @since 2024-08-02
+ */
+public class BJ_14890 {
+
+    static int n, l;
+    static int[][] map;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        l = Integer.parseInt(st.nextToken());
+
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (canRow(i)) {
+                count++;
+            }
+            if (canCol(i)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+
+    private static boolean canRow(int r) {
+
+        boolean[] incline = new boolean[n];
+
+        for (int i = 0; i < n - 1; i++) {
+            int diff = map[r][i] - map[r][i + 1];
+
+            if (Math.abs(diff) > 1) {
+                return false;
+            }
+
+            //내려가야 하는 경우
+            if (diff == 1) {
+                for (int j = 1; j <= l; j++) {
+
+                    if (i + j >= n || incline[i + j] || map[r][i] - 1 != map[r][i + j]) {
+                        return false;
+                    }
+
+                    incline[i + j] = true;
+                }
+            }
+            //올라가야 하는 경우
+            else if (diff == -1) {
+                for (int j = 0; j < l; j++) {
+
+                    if (i - j < 0 || incline[i - j] || map[r][i] != map[r][i - j]) {
+                        return false;
+                    }
+
+                    incline[i - j] = true;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean canCol(int c) {
+
+        boolean[] incline = new boolean[n]; //경사로 설치 여부
+
+        for (int i = 0; i < n - 1; i++) {
+            int diff = map[i][c] - map[i + 1][c];
+
+            if (Math.abs(diff) > 1) {
+                return false;
+            }
+
+            //내려가야 하는 경우
+            if (diff == 1) {
+                for (int j = 1; j <= l; j++) {
+
+                    if (i + j >= n || incline[i + j] || map[i][c] - 1 != map[i + j][c]) {
+                        return false;
+                    }
+
+                    incline[i + j] = true;
+
+                }
+            }
+            //올라가야 하는 경우
+            else if (diff == -1) {
+                for (int j = 0; j < l; j++) {
+
+                    if (i - j < 0 || incline[i - j] || map[i][c] != map[i - j][c]) {
+                        return false;
+                    }
+
+                    incline[i - j] = true;
+                }
+            }
+        }
+
+        return true;
+    }
+}
