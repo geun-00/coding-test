9feabd75512d0e8f7e9c183918b0cf0dce6942ad
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-08-06 20:45:49 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 8.10 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -19,16 +19,18 @@ class Solution {
             }
         }
         
+        //0, 0에서 시작하면 직선과 코너를 구분하기 애매하다고 생각해 한 칸 이동한 상태에서 탐색 시작
         if (board[0][1] == 0) {
-            qu.offer(new Car(0, 1, 100, 3));
+            qu.offer(new Car(0, 1, 100, 3)); //오른쪽
             cost[0][1][3] = 100;
         }
         if (board[1][0] == 0) {
-            qu.offer(new Car(1, 0, 100, 1));
+            qu.offer(new Car(1, 0, 100, 1)); //아래
             cost[1][0][1] = 100;
         }
         
         while (!qu.isEmpty()) {
+            
             Car now = qu.poll();
 
             if (now.x == n - 1 && now.y == n - 1) {
@@ -36,15 +38,19 @@ class Solution {
                 continue;
             }
 
+            //0=위, 1=아래, 2=왼쪽, 3=오른쪽
             for (int i = 0; i < 4; i++) {
                 int nx = now.x + dx[i];
                 int ny = now.y + dy[i];
 
                 if (nx >= 0 && ny >= 0 && nx < n && ny < n && board[nx][ny] == 0) {
+                    
                     int newCost = now.cost + 100;
+                    //코너 추가 비용
                     if (now.dir != i) {
                         newCost += 500;
                     }
+                    //현재 방향으로의 이동이 최소 비용일 때만 유망한 경로라고 판단하고 큐에 추가
                     if (cost[nx][ny][i] > newCost) {
                         cost[nx][ny][i] = newCost;
                         qu.offer(new Car(nx, ny, newCost, i));
@@ -58,9 +64,9 @@ class Solution {
 }
 
 class Car {
-    int x, y;
-    int cost;
-    int dir;
+    int x, y; //위치
+    int cost; //현재 비용
+    int dir; //이동 방향
 
     public Car(int x, int y, int cost, int dir) {
         this.x = x;
