d27479bb4d9a14aab2df1a0f9cc96b2a4e83aef1
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-08-15 16:34:32 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 40.93 ms, Memory: 215 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -22,15 +22,17 @@ class Solution {
         
         }
 
-        for (int x = 0; x < n; x++) {
-            for (int y = 1; y < m; y++) {
-                arr[x][y] += arr[x][y - 1];
-            }
+        for (int i = 1; i < m; i++) {
+            arr[0][i] += arr[0][i - 1];
         }
 
-        for (int y = 0; y < m; y++) {
-            for (int x = 1; x < n; x++) {
-                arr[x][y] += arr[x - 1][y];
+        for (int i = 1; i < n; i++) {
+            arr[i][0] += arr[i - 1][0];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                arr[i][j] = arr[i - 1][j] + arr[i][j - 1] - arr[i - 1][j - 1] + arr[i][j];
             }
         }
 
