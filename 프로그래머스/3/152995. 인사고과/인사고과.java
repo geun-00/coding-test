355586278bd7369ffbe19e8e4c3844d108bf94dc
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-08-16 10:03:44 +0900

    [level 3] Title: 인사고과, Time: 145.72 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -13,13 +13,17 @@ class Solution {
             return o2[0] - o1[0];
         });
 
+        int result = 1;
+
         int max = scores[0][1];
 
-        for (int i = 1; i < scores.length; i++) {
+        for (int i = 0; i < scores.length; i++) {
+
+            int sum = scores[i][0] + scores[i][1];
 
             if (max > scores[i][1]) {
 
-                if (scores[i][0] == wanho[0] && scores[i][1] == wanho[1]) {
+                if (Arrays.equals(scores[i], wanho)) {
                     return -1;
                 }
 
@@ -27,28 +31,13 @@ class Solution {
                 scores[i][1] = 0;
             } else {
                 max = scores[i][1];
+
+                if (sum > wanhoSum) {
+                    result++;
+                }
             }
         }
-
-        Arrays.sort(scores,(o1, o2) -> {
-            int sum1 = o1[0] + o1[1];
-            int sum2 = o2[0] + o2[1];
-
-            return sum2 - sum1;
-        });
-
-        int result = 1;
-
-
-        for (int[] score : scores) {
-            int sum = score[0] + score[1];
-
-            if (sum > wanhoSum) {
-                result++;
-            } else {
-                return result;
-            }
-        }
-        return -1;
+        
+        return result;
     }
 }
