cecb37cf46cf94758b0f392c5d5f3593d5588c49
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-09-15 09:04:13 +0900

    [Gold II] Title: 트리, Time: 380 ms, Memory: 47204 KB -BaekjoonHub

--- 백준/Gold/4256. 트리/트리.java
+++ 백준/Gold/4256. 트리/트리.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
@@ -11,6 +10,7 @@ public class Main {
     static int[] inOrder;
     static int[] postOrder;
     static int index;
+    static StringBuilder sb = new StringBuilder();
 
     static HashMap<Integer, Integer> inOrderMap;
 
@@ -20,8 +20,6 @@ public class Main {
         StringTokenizer st;
 
         int t = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
 
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
@@ -48,9 +46,6 @@ public class Main {
             postOrder = new int[n];
             solve(0, n - 1, 0, n - 1);
 
-            for (int node : postOrder) {
-                sb.append(node).append(" ");
-            }
             sb.append("\n");
         }
 
@@ -58,6 +53,7 @@ public class Main {
     }
 
     private static void solve(int pre_s, int pre_e, int in_s, int in_e) {
+
         if (pre_s > pre_e || in_s > in_e) {
             return;
         }
@@ -71,6 +67,6 @@ public class Main {
         solve(pre_s + 1, pre_s + leftSize, in_s, rootIndex - 1);
         solve(pre_s + leftSize + 1, pre_e, rootIndex + 1, in_e);
 
-        postOrder[index++] = root;
+        sb.append(root).append(" ");
     }
 }
