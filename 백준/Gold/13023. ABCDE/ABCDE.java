70f82e4ced2dc1e1eda4086d3ea3b756ef395331
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-09-23 08:29:01 +0900

    [Gold V] Title: ABCDE, Time: 224 ms, Memory: 21088 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static ArrayList<Integer>[] graph;
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        visit = new boolean[n];
+        graph = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 0; i < n; i++) {
+            dfs(i, 0);
+        }
+
+        System.out.println(0);
+    }
+
+    private static void dfs(int num, int depth) {
+        if (depth == 4) {
+            System.out.println(1);
+            System.exit(0);
+        }
+
+        visit[num] = true;
+
+        for (int next : graph[num]) {
+            if (!visit[next]) {
+                dfs(next, depth + 1);
+            }
+        }
+
+        visit[num] = false;
+    }
+}
