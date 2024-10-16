d5d2579634dc8078c19cf7b7468f008f5d508b5d
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-10-16 19:33:29 +0900

    [D1] Title: 알파벳을 숫자로 변환, Time: 169 ms, Memory: 20,100 KB -BaekjoonHub

--- SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.java
+++ SWEA/D1/2050. 알파벳을 숫자로 변환/알파벳을 숫자로 변환.java
@@ -10,15 +10,8 @@ class Solution
 		
         String s = sc.next();
         
-        char[] alpha = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < 26; i++) {
-            map.put(alpha[i], i + 1);
-        }
-
-        for (int i = 0; i < s.length(); i++) {
-            System.out.print(map.get(s.charAt(i)) + " ");
+       for (int i = 0; i < s.length(); i++) {
+            System.out.print((s.charAt(i) - 'A' + 1) + " ");
         }
 	}
 }
