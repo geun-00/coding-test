c988f2e219b2457397cfeed4ebf5be0d4cb4a89d
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-10-19 09:16:01 +0900

    [Gold I] Title: 냅색문제, Time: 208 ms, Memory: 20152 KB -BaekjoonHub

--- 백준/Gold/1450. 냅색문제/냅색문제.java
+++ 백준/Gold/1450. 냅색문제/냅색문제.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,6 +15,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken()); //물건의 개수
@@ -23,48 +28,61 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        ArrayList<Long> left = new ArrayList<>();
-        ArrayList<Long> right = new ArrayList<>();
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
 
         solve(0, n / 2, 0, left);
         solve(n / 2, n, 0, right);
 
         long count = 0;
-        
-        right.sort(null);
+
+        Collections.sort(right);
 
         for (long num : left) {
             count += upperBound(c - num, right);
         }
 
-        System.out.println(count);
+        bw.write(String.valueOf(count));
+        bw.flush();
+
+        bw.close();
+        br.close();
     }
 
-    private static long upperBound(long target, ArrayList<Long> list) {
+    private static int upperBound(long target, ArrayList<Integer> list) {
 
         int left = 0;
-        int right = list.size();
+        int right = list.size() - 1;
+        int mid;
 
-        while (left < right) {
-            int mid = (left + right) / 2;
+        while (left <= right) {
+
+            mid = (left + right) / 2;
+
             if (list.get(mid) <= target) {
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid - 1;
             }
         }
         return left;
     }
 
-    private static void solve(int s, int e, long sum, ArrayList<Long> list) {
+    private static void solve(int s, int e, int sum, ArrayList<Integer> list) {
+
         if (sum > c) {
             return;
         }
+
         if (s == e) {
             list.add(sum);
             return;
         }
+
+        //현재 물건을 넣지 않는 경우
         solve(s + 1, e, sum, list);
+
+        //현재 물건을 넣는 경우
         solve(s + 1, e, sum + arr[s], list);
     }
 }
