c95ccbce3790c8102a6a95b5cfb4f44942fc6467
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-02 15:43:42 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1552 ms, Memory: 346288 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        PriorityQueue<Edge> qu = new PriorityQueue<>();
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            qu.offer(new Edge(a, b, c));
+            qu.offer(new Edge(b, a, c));
+        }
+
+        int edge = 0;
+        int cost = 0;
+        int max = 0;
+
+        while (!qu.isEmpty()) {
+
+            Edge e = qu.poll();
+
+            if (find(e.from) != find(e.to)) {
+                union(e.from, e.to);
+                edge++;
+                cost += e.weight;
+                max = e.weight;
+            }
+
+            if (edge == n - 1) {
+                break;
+            }
+        }
+
+        System.out.println(cost - max);
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
+    static class Edge implements Comparable<Edge> {
+        int from, to, weight;
+
+        public Edge(int from, int to, int weight) {
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.weight - o.weight;
+        }
+    }
+}
