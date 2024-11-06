4d2a15e5edf2cb562d9b6ef10c6ca019decb2989
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-06 17:06:57 +0900

    [D3] Title: 두가지 빵의 딜레마, Time: 103 ms, Memory: 19,392 KB -BaekjoonHub

---
+++ SWEA/D3/5162. 두가지 빵의 딜레마/두가지 빵의 딜레마.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc <= t ; tc++) {
+			
+			sb.append("#").append(tc).append(" ");			
+								
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			sb.append(c / Math.min(a,  b));
+			
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+}
