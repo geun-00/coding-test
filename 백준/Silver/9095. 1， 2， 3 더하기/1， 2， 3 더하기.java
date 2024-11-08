e4ad40be40921e25d26644452bbc897608c50eff
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-08 10:18:07 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            int[] dp = new int[11];
+
+            dp[1] = 1;
+            dp[2] = 2;
+            dp[3] = 4;
+
+            for (int i = 4; i <= n; i++) {
+                dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+            }
+
+            sb.append(dp[n]).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
