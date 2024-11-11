51fe676b35be1127a7a6bf5f4aab3f60eff36d7a
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-11 11:18:33 +0900

    [Silver I] Title: 회전 초밥, Time: 168 ms, Memory: 18708 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        int[] sushi = new int[d + 1];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            if (sushi[arr[i]] == 0) {
+                count++;
+            }
+            sushi[arr[i]]++;
+        }
+
+        int max = count + (sushi[c] == 0 ? 1 : 0);
+
+        for (int i = 1; i < n; i++) {
+
+            int addIdx = (i + k - 1) % n;
+            int removeIdx = i - 1;
+
+            sushi[arr[removeIdx]]--;
+            if (sushi[arr[removeIdx]] == 0) {
+                count--;
+            }
+
+            if (sushi[arr[addIdx]] == 0) {
+                count++;
+            }
+            sushi[arr[addIdx]]++;
+
+            max = Math.max(max, count + (sushi[c] == 0 ? 1 : 0));
+        }
+
+        System.out.println(max);
+    }
+}
