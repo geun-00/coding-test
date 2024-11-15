b25f80b4298a2a8d6fad0b6f25c8597e48b1a22c
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-15 11:56:26 +0900

    [Silver IV] Title: 덱, Time: 172 ms, Memory: 18824 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        while (n-- > 0) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String op = st.nextToken();
+
+            int x;
+
+            switch (op) {
+                case "push_front":
+                    x = Integer.parseInt(st.nextToken());
+                    deque.offerFirst(x);
+                    break;
+                case "push_back":
+                    x = Integer.parseInt(st.nextToken());
+                    deque.offerLast(x);
+                    break;
+                case "pop_front":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollFirst()).append("\n");
+                    break;
+                case "pop_back":
+                    sb.append(deque.isEmpty() ? -1 : deque.pollLast()).append("\n");
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(deque.isEmpty() ? 1 : 0).append("\n");
+                    break;
+                case "front":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekFirst()).append("\n");
+                    break;
+                case "back":
+                    sb.append(deque.isEmpty() ? -1 : deque.peekLast()).append("\n");
+                    break;
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
