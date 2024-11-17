c48324b665dc8fbe76f9fb1edfaaa2f3f56a940f
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-17 20:39:19 +0900

    [Silver IV] Title: 대칭 차집합, Time: 820 ms, Memory: 89572 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> a = new HashSet<>();
+        HashSet<Integer> b = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            b.add(Integer.parseInt(st.nextToken()));
+        }
+
+        HashSet<Integer> temp = new HashSet<>(a);
+        a.removeAll(b);
+        b.removeAll(temp);
+
+        System.out.println(a.size() + b.size());
+    }
+}
