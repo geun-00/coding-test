1f4d15d284afaa01431db26daba1ac8311527480
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-26 14:44:50 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 616 ms, Memory: 50016 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int[] a : arr) {
+            sb.append(a[0]).append(" ").append(a[1]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
