b149f5a4749175e7e923b149ad5b3e07db8564a1
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-26 12:53:04 +0900

    [level 2] Title: 삼각 달팽이, Time: 22.14 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int[] solution(int n) {
+        
+        int[] ans = new int[n * (n + 1) / 2];
+        
+        int[][] arr = new int[n][n];
+        
+        int[] dx = {1, 0, -1};
+        int[] dy = {0, 1, -1};
+        
+        int num = 1;
+        int x = 0;
+        int y = 0;
+        int dir = 0;
+        
+        while(true) {
+            
+            arr[x][y] = num++;
+            
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            
+            if(nx < 0 || ny < 0 || nx >= n || ny >= n || arr[nx][ny] != 0) {
+                
+                dir = (dir + 1) % 3;
+                nx = x + dx[dir];
+                ny = y + dy[dir];
+                
+                if(nx < 0 || ny < 0 || nx >= n || ny >= n || arr[nx][ny] != 0) {
+                    break;
+                }
+            }
+            
+            x = nx;
+            y = ny;
+        }
+        
+        int idx = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                ans[idx++] = arr[i][j];
+            }
+        }
+        
+        return ans;
+    }
+}
