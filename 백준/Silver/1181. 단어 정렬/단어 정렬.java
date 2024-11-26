e67859866b088f0906e3b55a6db6a5795cc4edaa
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-26 14:31:34 +0900

    [Silver V] Title: 단어 정렬, Time: 316 ms, Memory: 26452 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);
+            }
+            return a.length() - b.length();
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        Arrays.stream(arr).distinct().forEach(s -> sb.append(s).append("\n"));
+
+        System.out.print(sb);
+    }
+}
