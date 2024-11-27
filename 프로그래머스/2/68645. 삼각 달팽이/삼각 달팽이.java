8815fa45b0d521400d018dabb96ccc6e22bd6d13
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-27 09:39:07 +0900

    [level 2] Title: 삼각 달팽이, Time: 17.95 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -36,6 +36,7 @@ class Solution {
         }
         
         int idx = 0;
+        
         for(int i = 0; i < n; i++){
             for(int j = 0; j <= i; j++){
                 ans[idx++] = arr[i][j];
