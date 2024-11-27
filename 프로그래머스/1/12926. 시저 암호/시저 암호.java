5f9672f8f0dc23561ad2fd2e0dc0efd8fa598258
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-27 12:25:41 +0900

    [level 1] Title: 시저 암호, Time: 1.35 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,6 +1,21 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        return answer;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(char c : s.toCharArray()) {
+            
+            if(c == ' '){
+                sb.append(" ");
+                continue;
+            }
+            
+            int first = Character.isUpperCase(c) ? 'A' : 'a';
+            
+            sb.append((char) ((c - first + n) % 26 + first));
+            
+        }
+        
+        return sb.toString();
     }
 }
