4a88a44daf30c76002a3a6a8c6486ed45252090a
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-27 11:23:05 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.13 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class Solution {
+    
+    static int[] dx = {-1, 0, 0, 1};
+    static int[] dy = {0, -1, 1, 0};
+    
+    public int[] solution(String[][] places) {
+        
+        int[] ans = new int[5];
+        char[][] arr = new char[5][5];
+        
+        for(int i = 0; i < 5; i++) {
+            
+            String[] place = places[i];            
+            
+            for(int j = 0; j < 5; j++) {
+                arr[j] = place[j].toCharArray();                        
+            }
+            
+            ans[i] = check(arr);            
+        }
+        
+        return ans;
+    }
+    
+    public int check(char[][] arr) {
+        
+        for(int i = 0; i < 5; i++) {                                    
+            for(int j = 0; j < 5; j++) {
+                 
+                if(arr[i][j] != 'P') continue;
+                    
+                if(!check(arr, i, j)) return 0;                
+            }                  
+        }
+        
+        return 1;
+    }
+    
+    public boolean check(char[][] arr, int x, int y) {
+        
+        for(int i = 0; i < 4; i++) {
+            
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx < 0 || ny < 0 || nx >= 5 || ny >= 5 || arr[nx][ny] == 'X') continue;
+            
+            if(arr[nx][ny] == 'P') return false;
+            
+            if(check(arr, nx, ny, 3 - i)) return false;
+        }
+        
+        return true;
+    }
+    
+    public boolean check(char[][] arr, int x, int y, int excludeDir) {
+        
+        for(int i = 0; i < 4; i++) {
+            
+            if(i == excludeDir) continue;
+            
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx < 0 || ny < 0 || nx >= 5 || ny >= 5 || arr[nx][ny] == 'X') continue;
+            
+            if(arr[nx][ny] == 'P') return true;
+        }
+        
+        return false;
+    }
+}
