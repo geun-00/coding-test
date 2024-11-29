ef0c6a5eb8347a6b7959195471a9edbf0f2a7674
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-11-29 19:26:14 +0900

    [Gold I] Title: 로봇 청소기, Time: 436 ms, Memory: 17128 KB -BaekjoonHub

--- 백준/Gold/4991. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/4991. 로봇 청소기/로봇 청소기.java
@@ -8,14 +8,16 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] order;
-    static char[][] arr;
-    static int w, h;
-    static ArrayList<Integer> dirtyPos;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[] cleanerDist;
-    static int[][] dirtyDist;
+
+    static ArrayList<Integer> dirtyPos;     //더러운 칸의 위치
+    static int[] order;                     //더러운 칸을 이동하는 순서
+    static int[] cleanerDist;               //로봇 청소기에서 각 더러운 칸까지의 최단 경로
+    static int[][] dirtyDist;               //더러운 칸끼리 최단 경로
+
+    static char[][] arr;
+    static int w, h;
 
     static int ans;
 
@@ -26,6 +28,7 @@ public class Main {
 
         whileLoop:
         while (true) {
+
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             w = Integer.parseInt(st.nextToken());
@@ -38,10 +41,11 @@ public class Main {
             ans = Integer.MAX_VALUE;
             arr = new char[h][w];
 
-            int cleanerPos = 0;
             dirtyPos = new ArrayList<>();
+            int cleanerPos = 0;
 
             for (int i = 0; i < h; i++) {
+
                 char[] chars = br.readLine().toCharArray();
 
                 for (int j = 0; j < w; j++) {
@@ -51,12 +55,14 @@ public class Main {
 
                     if (arr[i][j] == 'o') {
                         cleanerPos = idx;
-                    } else if (arr[i][j] == '*') {
+                    }
+                    else if (arr[i][j] == '*') {
                         dirtyPos.add(idx);
                     }
                 }
             }
 
+            //처음부터 이동할 필요 없는 경우
             if (dirtyPos.isEmpty()) {
                 sb.append(0).append("\n");
                 continue;
@@ -67,11 +73,14 @@ public class Main {
             cleanerDist = new int[n];
             dirtyDist = new int[n][n];
 
+            //로봇 청소기 위치에서 각 더러운 칸까지 최단 경로를 구한다.
+            //만약 모든 더러운 칸을 방문할 수 없다면 -1 출력
             if (!bfs(cleanerPos)) {
                 sb.append(-1).append("\n");
                 continue;
             }
 
+            //더러운 칸끼리 최단 경로를 구한다.
             for (int i = 0; i < n - 1; i++) {
                 for (int j = i + 1; j < n; j++) {
 
@@ -80,6 +89,7 @@ public class Main {
 
                     int dist = bfs(start, target);
 
+                    //방문할 수 없다면 -1 출력
                     if (dist == -1) {
                         sb.append(-1).append("\n");
                         continue whileLoop;
@@ -90,44 +100,67 @@ public class Main {
             }
 
             order = new int[n];
+            //백트래킹으로 이동 순서에 대한 모든 조합 확인
             getCombinations(0, 0, n);
 
             sb.append(ans).append("\n");
-
         }
 
         System.out.print(sb);
     }
 
-    private static void getCombinations(int idx, int visit, int n) {
+    /**
+     * 로봇 청소기 위치에서 시작하는 BFS
+     * @param start 로봇 청소기 위치
+     * @return 모든 더러운 칸을 방문할 수 있는지 여부
+     */
+    private static boolean bfs(int start) {
 
-        if (idx == n) {
-            calculateMin(n);
-            return;
-        }
+        Queue<Integer> qu = new ArrayDeque<>();
+        qu.offer(start);
 
-        for (int i = 0; i < n; i++) {
+        boolean[] visit = new boolean[w * h];
+        visit[start] = true;
 
-            if ((visit & (1 << i)) != 0) {
-                continue;
+        int moved = 0;  //이동 횟수
+        int found = 0;  //더러운 칸 만난 횟수
+
+        while (!qu.isEmpty()) {
+
+            int size = qu.size();
+
+            for (int i = 0; i < size; i++) {
+
+                int now = qu.poll();
+                int x = now / w;
+                int y = now % w;
+
+                int idx = x * w + y;
+
+                if (dirtyPos.contains(idx)) {
+                    cleanerDist[dirtyPos.indexOf(idx)] = moved;
+                    found++;
+
+                    if (found == dirtyPos.size()) {
+                        return true;
+                    }
+                }
+
+                searchAdjNode(x, y, visit, qu);
             }
 
-            order[idx] = i;
-            getCombinations(idx + 1, visit | (1 << i), n);
-        }
-    }
-
-    private static void calculateMin(int n) {
-
-        int sum = cleanerDist[order[0]];
-
-        for (int i = 0; i < n - 1; i++) {
-            sum += dirtyDist[order[i]][order[i + 1]];
+            moved++;
         }
 
-        ans = Math.min(ans, sum);
+        return false;
     }
 
+    /**
+     * 각 더러운 칸에서 시작하는 BFS
+     * @param start 시작 더러운 칸
+     * @param target 방문할 더러운 칸
+     * @return 최소 이동 횟수, 방문할 수 없으면 -1
+     */
     private static int bfs(int start, int target) {
 
         Queue<Integer> qu = new ArrayDeque<>();
@@ -154,20 +187,7 @@ public class Main {
                     return moved;
                 }
 
-                for (int d = 0; d < 4; d++) {
-
-                    int nx = x + dx[d];
-                    int ny = y + dy[d];
-
-                    idx = nx * w + ny;
-
-                    if (rangeCheck(nx, ny) || visit[idx] || arr[nx][ny] == 'x') {
-                        continue;
-                    }
-
-                    visit[idx] = true;
-                    qu.offer(idx);
-                }
+                searchAdjNode(x, y, visit, qu);
             }
 
             moved++;
@@ -176,59 +196,49 @@ public class Main {
         return -1;
     }
 
-    private static boolean bfs(int start) {
+    private static void searchAdjNode(int x, int y, boolean[] visit, Queue<Integer> qu) {
 
-        Queue<Integer> qu = new ArrayDeque<>();
-        qu.offer(start);
+        for (int d = 0; d < 4; d++) {
 
-        boolean[] visit = new boolean[w * h];
-        visit[start] = true;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
 
-        int moved = 0;
-        int found = 0;
+            int idx = nx * w + ny;
 
-        while (!qu.isEmpty()) {
-
-            int size = qu.size();
-
-            for (int i = 0; i < size; i++) {
-
-                int now = qu.poll();
-                int x = now / w;
-                int y = now % w;
-
-                int idx = x * w + y;
-
-                if (dirtyPos.contains(idx)) {
-                    cleanerDist[dirtyPos.indexOf(idx)] = moved;
-                    found++;
-
-                    if (found == dirtyPos.size()) {
-                        return true;
-                    }
-                }
-
-
-                for (int d = 0; d < 4; d++) {
-
-                    int nx = x + dx[d];
-                    int ny = y + dy[d];
-
-                    idx = nx * w + ny;
-
-                    if (rangeCheck(nx, ny) || visit[idx] || arr[nx][ny] == 'x') {
-                        continue;
-                    }
-
-                    visit[idx] = true;
-                    qu.offer(idx);
-                }
+            if (rangeCheck(nx, ny) || visit[idx] || arr[nx][ny] == 'x') {
+                continue;
             }
 
-            moved++;
+            visit[idx] = true;
+            qu.offer(idx);
+        }
+    }
+
+    private static void getCombinations(int idx, int visit, int n) {
+
+        if (idx == n) {
+            calculateMin(n);
+            return;
         }
 
-        return false;
+        for (int i = 0; i < n; i++) {
+
+            if ((visit & (1 << i)) != 0) continue;
+
+            order[idx] = i;
+            getCombinations(idx + 1, visit | (1 << i), n);
+        }
+    }
+
+    private static void calculateMin(int n) {
+
+        int sum = cleanerDist[order[0]];
+
+        for (int i = 0; i < n - 1; i++) {
+            sum += dirtyDist[order[i]][order[i + 1]];
+        }
+
+        ans = Math.min(ans, sum);
     }
 
     private static boolean rangeCheck(int nx, int ny) {
