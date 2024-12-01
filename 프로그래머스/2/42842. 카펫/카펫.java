e9685e9d374ea3f5acb136e94b3cf6b41fc588ee
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-12-01 17:50:26 +0900

    [level 2] Title: 카펫, Time: 10.07 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,10 +2,19 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int half = (brown + 4) / 2;
 
-        for (int height = 3; height < half; height++) {
+        int low = 3;
+        int high = half / 2;
+
+        while (low <= high) {
+            int height = (low + high) / 2;
+
             int width = half - height;
 
-            if (width * height == brown + yellow) {
+            if (width * height < brown + yellow) {
+                low = height + 1;
+            } else if (width * height > brown + yellow) {
+                high = height - 1;
+            } else {
                 return new int[]{width, height};
             }
         }
