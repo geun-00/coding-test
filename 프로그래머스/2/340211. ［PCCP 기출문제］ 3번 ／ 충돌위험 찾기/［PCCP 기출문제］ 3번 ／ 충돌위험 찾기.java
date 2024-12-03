86a4c136144fc284258762d907eb5a3d28a9cc99
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-12-03 19:22:33 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 2401.00 ms, Memory: 406 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -0,0 +1,122 @@
+import java.util.*;
+
+class Solution {
+    
+    static final int R = 100;
+    static final int C = 100;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    
+    public int solution(int[][] points, int[][] routes) {
+        
+        int n = points.length;
+        
+        int[] point = new int[n];
+        
+        for(int i = 0; i < n; i++) {
+            int r = points[i][0] - 1;
+            int c = points[i][1] - 1;
+            
+            point[i] = r * C + c;
+        }
+        
+        int m = routes.length;
+        
+        Deque<Integer>[] route = new ArrayDeque[m];
+
+        for(int i = 0; i < m; i++) {
+
+            route[i] = new ArrayDeque<>();
+
+            for(int j = 0; j < routes[i].length - 1; j++) {
+                int start = routes[i][j] - 1;
+                int target = routes[i][j + 1] - 1;
+
+                bfs(point[start], point[target], route[i]);
+            }
+        }
+
+        boolean flag = true;
+        int ans = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while (flag) {
+
+            flag = false;
+            map.clear();
+
+            for (Queue<Integer> qu : route) {
+                if (!qu.isEmpty()) {
+                    flag = true;
+                    int pos = qu.poll();
+                    map.put(pos, map.getOrDefault(pos, 0) + 1);
+                }
+            }
+
+            for (int count : map.values()) {
+                if (count > 1) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+    
+    public void bfs(int start_idx, int target_idx, Deque<Integer> route) {
+        
+        int sx = start_idx / C;
+        int sy = start_idx % C;
+
+        boolean[][] visit = new boolean[R + 1][C + 1];
+        visit[sx][sy] = true;
+        
+        Queue<Integer> qu = new ArrayDeque<>();
+        qu.offer(start_idx);
+        
+        int[][] prev = new int[R + 1][C + 1];
+        
+        while(!qu.isEmpty()) {
+            
+            int now = qu.poll();
+            int x = now / C;
+            int y = now % C;
+            
+            if(now == target_idx) break;
+            
+            for(int i = 0; i < 4; i++) {
+                
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                
+                if(nx < 0|| ny < 0 || nx >= R || ny >= C || visit[nx][ny]) continue;
+                
+                visit[nx][ny] = true;
+                prev[nx][ny] = i;
+                qu.offer(nx * C + ny);                
+            }
+        }
+        
+        Deque<Integer> stk = new ArrayDeque<>();
+        
+        int x = target_idx / C;
+        int y = target_idx % C;
+        
+        while(!(x == sx && y == sy)) {
+            stk.push(x * C + y);
+            int dir = prev[x][y];
+            x -= dx[dir];
+            y -= dy[dir];
+        }
+        
+        if (!route.isEmpty() && route.peekLast() == start_idx) {
+            route.pollLast();
+        }
+
+        stk.push(start_idx);
+        
+        while(!stk.isEmpty()) {
+            route.offer(stk.pop());
+        }
+    }
+}
