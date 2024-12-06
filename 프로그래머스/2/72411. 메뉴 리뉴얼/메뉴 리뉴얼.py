39c161f316243e4226ff2fa528930de921d8db71
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-12-06 11:05:57 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 16.66 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,3 +1,45 @@
+from collections import defaultdict
+
 def solution(orders, course):
-    answer = []
-    return answer
+    
+    result = []
+    
+    for size in course:
+        
+        combs_count = defaultdict(int)
+        
+        for order in orders:
+            items = list(order)
+            
+            if len(items) < size:
+                continue
+            
+            items.sort()
+            solve(items, size, combs_count)
+        
+        max_count = 0
+        for count in combs_count.values():
+            if count >= 2 and count > max_count:
+                max_count = count
+        
+        for comb, count in combs_count.items():
+            if count == max_count:
+                result.append(comb)
+    
+    result.sort()
+    return result
+
+def solve(items, size, combs_count):
+    combs = []
+    get_combs(items, size, 0, "", combs)
+
+    for comb in combs:
+        combs_count[comb] += 1
+
+def get_combs(items, size, start, now, combs):
+    if size == 0:
+        combs.append(now)
+        return
+
+    for i in range(start, len(items)):
+        get_combs(items, size - 1, i + 1, now + items[i], combs)
