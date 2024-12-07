80377e57a96117529f7820cf166f9b3564025ffd
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-12-07 16:01:03 +0900

    [level 4] Title: 징검다리, Time: 41.64 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,6 +1,47 @@
+import java.util.*;
+
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
-        int answer = 0;
-        return answer;
+        
+        int[] arr = new int[rocks.length + 1];
+        
+        System.arraycopy(rocks, 0, arr, 0, rocks.length);
+        
+        arr[arr.length - 1] = distance;
+        
+        Arrays.sort(rocks);
+        
+        int low = 1;
+        int high = distance;
+        int ans = 0;
+        
+        while (low <= high) {
+            
+            int mid = (low + high) / 2;
+            
+            int removed = 0;
+            int last = 0;
+            
+            for(int rock : rocks) {
+                
+                if(rock - last < mid) {
+                    removed++;
+                    continue;
+                }
+                
+                last = rock;
+            }
+            
+            if(distance - last < mid) removed++;
+            
+            if(removed <= n) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return ans;
     }
 }
