7e7aeb3329d5a8af80f744a434bf984ef88caab4
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-12-11 12:32:41 +0900

    [Silver II] Title: 과자 나눠주기, Time: 696 ms, Memory: 257572 KB -BaekjoonHub

---
+++ 백준/Silver/16401. 과자 나눠주기/과자 나눠주기.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+arr = list(map(int, input().split()))
+
+low = 1
+high = 10**9
+ans = 0
+
+while low <= high:
+
+    mid = (low + high) // 2
+
+    sum = 0
+
+    for i in range(n):
+        sum += arr[i] // mid
+
+    if sum >= m:
+        ans = mid
+        low = mid + 1
+    else:
+        high = mid - 1
+
+print(ans)
