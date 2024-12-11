6f19aa75ce2e0745a6842483c44393d8dd65219d
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-12-11 10:34:10 +0900

    [level 2] Title: 올바른 괄호, Time: 17.82 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         
-        Stack<Character> stack = new Stack<>();
+        Deque<Character> stack = new ArrayDeque<>();
 
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '(') {
