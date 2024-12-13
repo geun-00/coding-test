826f71f8c3f7f7e055b7a69e9f1008c84271aa3d
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-12-13 11:59:22 +0900

    [level 3] Title: 길 찾기 게임, Time: 316.58 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/README.md
+++ 프로그래머스/3/42892. 길 찾기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 3] 길 찾기 게임 - 42892 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42892) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42892?language=python3) 
 
 ### 성능 요약
 
-메모리: 88.3 MB, 시간: 29.00 ms
+메모리: 14.9 MB, 시간: 316.58 ms
 
 ### 구분
 
@@ -12,11 +12,11 @@
 
 ### 채점결과
 
-정확성: 100.0<br/>효율성: 0.0<br/>합계: 100.0 / 100.0
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 제출 일자
 
-2024년 12월 13일 11:33:47
+2024년 12월 13일 11:59:21
 
 ### 문제 설명
 
--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.py
@@ -1,3 +1,69 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+class Node:
+    def __init__(self, num, x, y):
+        self.num = num
+        self.x = x
+        self.y = y
+        self.left = None
+        self.right = None
+
+        
+def build_tree(nodes):
+    root = nodes[0]
+    
+    for node in nodes[1:]:
+        insert(root, node)
+    
+    return root
+
+
+def insert(parent, node):
+    if(node.x < parent.x):
+        if not parent.left:
+            parent.left = node
+        else:
+            insert(parent.left, node)
+    else:
+        if not parent.right:
+            parent.right = node
+        else:
+            insert(parent.right, node)
+
+            
+def get_pre_order(node, pre_order):            
+    if not node:
+        return
+    
+    pre_order.append(node.num)
+    get_pre_order(node.left, pre_order)
+    get_pre_order(node.right, pre_order)
+    
+    
+def get_post_order(node, post_order):
+    if not node:
+        return
+    
+    get_post_order(node.left, post_order)
+    get_post_order(node.right, post_order)
+    post_order.append(node.num)
+    
+
 def solution(nodeinfo):
-    answer = [[]]
-    return answer
+    
+    n = len(nodeinfo)
+    
+    nodes = [Node(i + 1, nodeinfo[i][0], nodeinfo[i][1]) for i in range(n)]
+    
+    nodes.sort(key=lambda node: (-node.y, node.x))
+    
+    root = build_tree(nodes)
+    
+    pre_order = []
+    post_order = []
+    
+    get_pre_order(root, pre_order)
+    get_post_order(root, post_order)
+    
+    return [pre_order, post_order]
