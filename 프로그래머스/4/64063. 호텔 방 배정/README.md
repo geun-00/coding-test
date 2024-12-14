e16a3b688f76351b604ad6a62ec3dd84a36e2843
genesis <138780449+genesis12345678@users.noreply.github.com>
2024-12-14 13:54:32 +0900

    [level 4] Title: 호텔 방 배정, Time: 427.44 ms, Memory: 46.9 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/README.md
+++ 프로그래머스/4/64063. 호텔 방 배정/README.md
@@ -1,10 +1,10 @@
 # [level 4] 호텔 방 배정 - 64063 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64063?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64063?language=python3) 
 
 ### 성능 요약
 
-메모리: 170 MB, 시간: 448.53 ms
+메모리: 46.9 MB, 시간: 427.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 14일 13:28:00
+2024년 12월 14일 13:54:30
 
 ### 문제 설명
 
--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.py
@@ -1,3 +1,31 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+parent = {}
+
+def find(num):
+    
+    if num not in parent:
+        return num
+    
+    root = find(parent[num])
+    parent[num] = root
+    return root
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    
+    if a != b:
+        parent[a] = b
+
 def solution(k, room_number):
     answer = []
+    
+    for num in room_number:
+        found = find(num)
+        answer.append(found)
+        
+        union(num, found + 1)
+    
     return answer
