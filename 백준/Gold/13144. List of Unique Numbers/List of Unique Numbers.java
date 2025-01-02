a7c37ab66939dfa5d7948accc610ac6233fbd554
genesis <138780449+genesis12345678@users.noreply.github.com>
2025-01-02 09:28:51 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 264 ms, Memory: 24992 KB -BaekjoonHub

--- 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,26 +9,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] arr = Arrays.stream(br.readLine()
-                                    .split(" "))
-                          .mapToInt(Integer::parseInt)
-                          .toArray();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
         boolean[] visit = new boolean[100_001];
+
         int left = 0, right = 0;
         long ans = 0;
 
-        while (left < n || right < n) {
+        while (right < n) {
 
-            while (right < n && !visit[arr[right]]) {
+            if (!visit[arr[right]]) {
                 visit[arr[right]] = true;
+                ans += (right - left + 1);
                 right++;
+            } else {
+                visit[arr[left]] = false;
+                left++;
             }
-
-            ans += (right - left);
-
-            visit[arr[left]] = false;
-            left++;
         }
 
         System.out.println(ans);
