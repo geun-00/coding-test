1988dc3462989c2f8486eb740884aa9a4df3d355
genesis <138780449+genesis12345678@users.noreply.github.com>
2025-01-16 17:40:52 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 47.35 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -3,7 +3,7 @@ import java.util.regex.*;
 
 class Solution {
     public String[] solution(String[] files) {
-        String[] ans = new String[files.length];
+                
         File[] arr = new File[files.length];
 
         for (int i = 0; i < files.length; i++) {
@@ -19,20 +19,18 @@ class Solution {
             }
         }
 
-        Arrays.sort(arr, (f1, f2) -> {
-            if (!f1.head.equalsIgnoreCase(f2.head)) {
-                return f1.head.compareToIgnoreCase(f2.head);
-            }
-            int num1 = Integer.parseInt(f1.number);
-            int num2 = Integer.parseInt(f2.number);
-            return Integer.compare(num1, num2);
-        });
-
-        for (int i = 0; i < arr.length; i++) {
-            ans[i] = arr[i].toString();
-        }
-
-        return ans;
+        return Arrays.stream(arr)
+                     .sorted((f1, f2) -> {
+                         if (!f1.head.equalsIgnoreCase(f2.head)) {
+                             return f1.head.compareToIgnoreCase(f2.head);
+                         }
+                         return Integer.compare(
+                             Integer.parseInt(f1.number),
+                             Integer.parseInt(f2.number)
+                         );
+                     })
+                     .map(File::toString)
+                     .toArray(String[]::new);
     }
 
     static class File {
