057bae63626fcaa3838b0cc72c235ac3ee7e2f95
genesis <138780449+genesis12345678@users.noreply.github.com>
2025-01-18 18:45:07 +0900

    [Gold V] Title: 용액 합성하기, Time: 296 ms, Memory: 25608 KB -BaekjoonHub

--- 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
+++ 백준/Gold/14921. 용액 합성하기/용액 합성하기.java
@@ -1,26 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static int[] temp;
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        temp = new int[n];
 
-        int[] arr = Arrays.stream(br.readLine().split(" "))
-                          .mapToInt(Integer::parseInt)
-                          .toArray();
+        int[] arr = new int[n];
 
-        Arrays.sort(arr);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
 
         int ans = Integer.MAX_VALUE;
+
         int left = 0;
         int right = n - 1;
 
@@ -28,17 +27,12 @@ public class Main {
 
             int diff = arr[right] + arr[left];
 
-            if (Math.abs(diff) < Math.abs(ans)) {
-                ans = diff;
-            }
+            if (Math.abs(diff) < Math.abs(ans)) ans = diff;
 
             if (diff == 0) break;
 
-            if (diff < 0) {
-                left++;
-            } else {
-                right--;
-            }
+            if (diff < 0) left++;
+            else right--;
         }
 
         System.out.println(ans);
