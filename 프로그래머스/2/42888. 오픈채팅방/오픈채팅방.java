608d60d06454cd063fed10566b339f846a52173c
genesis <138780449+genesis12345678@users.noreply.github.com>
2025-01-19 09:31:05 +0900

    [level 2] Title: 오픈채팅방, Time: 290.97 ms, Memory: 290 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,32 +1,66 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 class Solution {
-    public String[] solution(String[] record) {
-        Map<String, String> map = new HashMap<>();
+    private static final String ENTER_FORMAT = "%s님이 들어왔습니다.";
+    private static final String LEAVE_FORMAT = "%s님이 나갔습니다.";
 
-        for (String r : record) {
-            String[] arr = r.split(" ");
+    private HashMap<String, UserInfo> userMap = new HashMap<>();
 
-            String command = arr[0];
-            if (command.equals("Enter") || command.equals("Change")) {
-                map.put(arr[1], arr[2]);
+    private class UserInfo {
+        public String userId;
+        public String nickName;
+
+        public UserInfo(String userId, String nickName) {
+            this.userId = userId;
+            this.nickName = nickName;
+        }
+
+    }
+
+    private class Command {
+        public char command;
+        public String userId;
+
+        public Command(char command, String userName) {
+            this.command = command;
+            this.userId = userName;
+        }
+    }
+
+
+    public String[] solution(String[] records) {
+        ArrayList<Command> commandList = new ArrayList<>();
+
+        for (String record : records) {
+            String[] split = record.split(" ");
+            String command = split[0];
+            String userId = split[1];
+            String nickName = null;
+
+            switch(command.charAt(0)) {
+                case 'E': // Enter
+                    nickName = split[2];
+                    if(userMap.containsKey(userId) == false) {
+                        userMap.put(userId, new UserInfo(userId, nickName));
+                    } else {
+                        userMap.get(userId).nickName = nickName;
+                    }
+
+                    commandList.add(new Command(command.charAt(0), userId));
+                    break;
+                case 'L': // Leave
+                    commandList.add(new Command(command.charAt(0), userId));
+                    break;
+                case 'C': // Change
+                    nickName = split[2];
+                    userMap.get(userId).nickName = nickName;
+                    break;
             }
         }
 
-        List<String> ans = new ArrayList<>();
-
-        for (String r : record) {
-            String[] arr = r.split(" ");
-
-            String command = arr[0];
-            if (command.equals("Enter")) {
-                ans.add(map.get(arr[1]) + "님이 들어왔습니다.");
-            } else if (command.equals("Leave")) {
-                ans.add(map.get(arr[1]) + "님이 나갔습니다.");
-            }
-        }
-
-        
-        return ans.toArray(String[]::new);
+        return commandList.stream()
+                .map(cmd -> String.format( cmd.command == 'E' ? ENTER_FORMAT : LEAVE_FORMAT , userMap.get(cmd.userId).nickName))
+                .toArray(ary -> new String[commandList.size()]);
     }
 }
