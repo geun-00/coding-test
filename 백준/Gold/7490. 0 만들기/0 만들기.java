3b535c2de735f783c03223fb123d56f254a5b7b9
genesis <138780449+genesis12345678@users.noreply.github.com>
2025-01-21 19:42:52 +0900

    [Gold V] Title: 0 만들기, Time: 152 ms, Memory: 17040 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,11 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Main {
+
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
 
@@ -13,67 +12,27 @@ public class Main {
 
         int t = Integer.parseInt(br.readLine());
 
-        StringBuilder sb = new StringBuilder();
-
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
 
-            int[] arr = new int[n];
-            for (int j = 0; j < n; j++) {
-                arr[j] = j + 1;
-            }
-
-            List<String> ans = new ArrayList<>();
-            solve(arr, 0, "", ans);
-
-            ans.stream()
-               .distinct()
-               .sorted()
-               .forEach(expr -> sb.append(expr).append("\n"));
+            solve(1, 1, 0, 1, "1", n);
             sb.append("\n");
         }
 
         System.out.print(sb);
     }
 
-    private static void solve(int[] arr, int index, String expr, List<String> ans) {
-        if (index == arr.length) {
-
-            expr = expr.substring(0, expr.length() - 1);
-
-            String temp = expr.replaceAll(" ", "");
-            StringTokenizer st = new StringTokenizer(temp, "+-", true);
-
-            int result = 0;
-            String lastOperation = "+";
-
-            while (st.hasMoreTokens()) {
-                String str = st.nextToken();
-
-                if (!str.equals("+") && !str.equals("-")) {
-                    result = calculate(result, lastOperation, str);
-                } else {
-                    lastOperation = str;
-                }
+    private static void solve(int index, int num, int sum, int operator, String expr, int n) {
+        if (index == n) {
+            sum += (num * operator);
+            if (sum == 0) {
+                sb.append(expr).append("\n");
             }
-
-            if (result == 0) {
-                ans.add(expr);
-            }
-
             return;
         }
 
-        solve(arr, index + 1, expr + arr[index] + "+", ans);
-        solve(arr, index + 1, expr + arr[index] + "-", ans);
-        solve(arr, index + 1, expr + arr[index] + " ", ans);
-    }
-
-    private static int calculate(int result, String lastOperation, String c) {
-        int num = Integer.parseInt(c);
-        if (lastOperation.equals("+")) {
-            return result + num;
-        }
-        return result - num;
+        solve(index + 1, num * 10 + (index + 1), sum, operator, expr + " " + (index + 1), n);
+        solve(index + 1, index + 1, sum + (num * operator), 1, expr + "+" + (index + 1), n);
+        solve(index + 1, index + 1, sum + (num * operator), -1, expr + "-" + (index + 1), n);
     }
 }
