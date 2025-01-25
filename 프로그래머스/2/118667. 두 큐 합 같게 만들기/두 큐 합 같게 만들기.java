4ae0043fcbd2aad215a0786c43e81bb2b8514bd0
genesis <138780449+genesis12345678@users.noreply.github.com>
2025-01-25 21:53:07 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 39.65 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,37 +1,39 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] queue1, int[] queue2) {
+        
         int n = queue1.length;
 
-        long sum1 = 0, sum2 = 0;
-        int[] arr = new int[n * 2];
+        long current = 0;
+        long total = 0;
+
+        Queue<Integer> qu1 = new ArrayDeque<>();
+        Queue<Integer> qu2 = new ArrayDeque<>();
 
         for (int i = 0; i < n; i++) {
-            sum1 += queue1[i];
-            sum2 += queue2[i];
+            current += queue1[i];
+            total += queue1[i] + queue2[i];
 
-            arr[i] = queue1[i];
-            arr[i + n] = queue2[i];
+            qu1.offer(queue1[i]);
+            qu2.offer(queue2[i]);
         }
 
-        long total = sum1 + sum2;
-        
         if (total % 2 != 0) return -1;
 
         long target = total / 2;
 
-        int i = 0, j = n;
-        long current = sum1;
         int ans = 0;
+        while (ans <= n * 4) {
 
-        while (i < n * 2 && j < n * 2) {
-            if (current == target) {
-                return ans;
-            }
+            if (current == target) return ans;
 
             if (current < target) {
-                current += arr[j++];
+                current += qu2.peek();
+                qu1.offer(qu2.poll());
             } else {
-                current -= arr[i++];
+                current -= qu1.peek();
+                qu2.offer(qu1.poll());
             }
 
             ans++;
