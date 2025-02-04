cd3b67d4a84c6f544c9ca21724117389647a566c
genesis <138780449+genesis12345678@users.noreply.github.com>
2025-02-04 09:32:21 +0900

    [Silver II] Title: 숫자판 점프, Time: 160 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[][] board = new int[5][5];
+    static Set<String> set = new HashSet<>();
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < 5; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                dfs(board[i][j] + "", i, j, 1);
+            }
+        }
+
+        System.out.println(set.size());
+    }
+
+    private static void dfs(String num, int x, int y, int depth) {
+        if (depth == 6) {
+            set.add(num);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= 5 || ny >= 5) {
+                continue;
+            }
+
+            dfs(num + board[nx][ny], nx, ny, depth + 1);
+        }
+    }
+}
