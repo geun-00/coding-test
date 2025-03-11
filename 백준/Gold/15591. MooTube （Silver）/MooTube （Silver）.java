bee6a83cd2d77e5433c63bbdf2d69aeb3ef6ef6b
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-03-11 14:56:43 +0900

    [Gold V] Title: MooTube (Silver), Time: 1276 ms, Memory: 311836 KB -BaekjoonHub

---
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        List<Node>[] graph = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int p = Integer.parseInt(st.nextToken()) - 1;
+            int q = Integer.parseInt(st.nextToken()) - 1;
+            int r = Integer.parseInt(st.nextToken());
+
+            graph[p].add(new Node(q, r));
+            graph[q].add(new Node(p, r));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int k = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken()) - 1;
+
+            boolean[] visit = new boolean[N];
+            Queue<Node> qu = new ArrayDeque<>();
+            int ans = 0;
+
+            visit[v] = true;
+            for (Node node : graph[v]) {
+                visit[node.to] = true;
+                qu.offer(new Node(node.to, node.w));
+            }
+
+            while (!qu.isEmpty()) {
+                Node now = qu.poll();
+
+                if (now.w >= k) {
+                    ans++;
+
+                    for (Node node : graph[now.to]) {
+                        if (!visit[node.to]) {
+                            visit[node.to] = true;
+                            qu.offer(new Node(node.to, Math.min(now.w, node.w)));
+                        }
+                    }
+                }
+            }
+
+            sb.append(ans).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static class Node {
+        int to, w;
+
+        public Node(int to, int w) {
+            this.to = to;
+            this.w = w;
+        }
+    }
+}
