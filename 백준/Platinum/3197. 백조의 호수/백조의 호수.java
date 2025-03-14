ef5e713cd41bbfadd780ca0ddd9a87288a1cac24
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-03-14 14:02:08 +0900

    [Platinum V] Title: 백조의 호수, Time: 988 ms, Memory: 160092 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.java
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.java
@@ -11,7 +11,8 @@ public class Main {
     static char[][] map;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[][] parent;
+    static Queue<Point> waterQu, nextWaterQu;
+    static Queue<Point> swanQu, nextSwanQu;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,60 +21,44 @@ public class Main {
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
 
+        waterQu = new ArrayDeque<>();
+        nextWaterQu = new ArrayDeque<>();
+        swanQu = new ArrayDeque<>();
+        nextSwanQu = new ArrayDeque<>();
+
         map = new char[r][c];
-        parent = new int[r][c];
         Point[] swans = new Point[2];
-        Queue<Point> waterQu = new ArrayDeque<>();
         int index = 0;
 
         for (int i = 0; i < r; i++) {
-            map[i] = br.readLine()
-                       .toCharArray();
+            map[i] = br.readLine().toCharArray();
 
             for (int j = 0; j < c; j++) {
-                parent[i][j] = i * c + j;
-
                 if (map[i][j] == 'L') {
                     swans[index++] = new Point(i, j);
                     waterQu.offer(new Point(i, j));
+                    map[i][j] = '.';
                 } else if (map[i][j] == '.') {
                     waterQu.offer(new Point(i, j));
                 }
             }
         }
 
-        for (int x = 0; x < r; x++) {
-            for (int y = 0; y < c; y++) {
-                if (map[x][y] == 'X') continue;
+        Point p = swans[0];
+        swanQu.offer(p);
 
-                for (int i = 0; i < 4; i++) {
-                    int nx = x + dx[i];
-                    int ny = y + dy[i];
-
-                    if (nx < 0 || ny < 0 || nx >= r || ny >= c || map[nx][ny] == 'X') continue;
-
-                    union(x, y, nx, ny);
-                }
-            }
-        }
-
-        int swan1 = find(swans[0].x, swans[0].y);
-        int swan2 = find(swans[1].x, swans[1].y);
+        boolean[][] visit = new boolean[r][c];
+        visit[p.x][p.y] = true;
 
         int days = 0;
-        while (swan1 != swan2) {
+        while (!canMeet(visit, swans[1].x, swans[1].y)) {
+            melting();
             days++;
-            waterQu = meltAndUnion(waterQu);
-            swan1 = find(swans[0].x, swans[0].y);
-            swan2 = find(swans[1].x, swans[1].y);
         }
-
         System.out.println(days);
     }
 
-    private static Queue<Point> meltAndUnion(Queue<Point> waterQu) {
-        Queue<Point> temp = new ArrayDeque<>();
-
+    private static void melting() {
         while (!waterQu.isEmpty()) {
             Point p = waterQu.poll();
             int x = p.x;
@@ -83,39 +68,49 @@ public class Main {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if (nx < 0 || ny < 0 || nx >= r || ny >= c || map[nx][ny] != 'X') continue;
+                if (nx < 0 || ny < 0 || nx >= r || ny >= c || map[nx][ny] != 'X') {
+                    continue;
+                }
 
+                nextWaterQu.offer(new Point(nx, ny));
                 map[nx][ny] = '.';
-                temp.offer(new Point(nx, ny));
+            }
+        }
 
-                for (int d = 0; d < 4; d++) {
-                    int nnx = nx + dx[d];
-                    int nny = ny + dy[d];
+        waterQu = nextWaterQu;
+        nextWaterQu = new ArrayDeque<>();
+    }
 
-                    if (nnx < 0 || nny < 0 || nnx >= r || nny >= c || map[nnx][nny] == 'X') continue;
+    private static boolean canMeet(boolean[][] visit, int ex, int ey) {
+        while (!swanQu.isEmpty()) {
+            Point p = swanQu.poll();
+            int x = p.x;
+            int y = p.y;
+            if (x == ex && y == ey) {
+                return true;
+            }
 
-                    union(nx, ny, nnx, nny);
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || ny < 0 || nx >= r || ny >= c || visit[nx][ny]) {
+                    continue;
+                }
+
+                visit[nx][ny] = true;
+
+                if (map[nx][ny] == '.') {
+                    swanQu.offer(new Point(nx, ny));
+                } else if (map[nx][ny] == 'X') {
+                    nextSwanQu.offer(new Point(nx, ny));
                 }
             }
         }
 
-        return temp;
-    }
-
-    private static void union(int x1, int y1, int x2, int y2) {
-        int num1 = find(x1, y1);
-        int num2 = find(x2, y2);
-
-        if (num1 != num2) {
-            parent[num2 / c][num2 % c] = num1;
-        }
-    }
-
-    private static int find(int x, int y) {
-        if (parent[x][y] == (x * c + y)) {
-            return parent[x][y];
-        }
-        return parent[x][y] = find(parent[x][y] / c, parent[x][y] % c);
+        swanQu = nextSwanQu;
+        nextSwanQu = new ArrayDeque<>();
+        return false;
     }
 
     static class Point {
