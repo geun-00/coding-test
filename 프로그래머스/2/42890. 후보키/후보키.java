2368c562f0114f046066008922e69e1ba47b6da1
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-03-15 11:27:17 +0900

    [level 2] Title: 후보키, Time: 7.70 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] relation) {
+        int col = relation[0].length;
+
+        List<List<Integer>> candidateKeys = new ArrayList<>();
+
+        iter:
+        for (int i = 1; i < (1 << col) - 1; i++) {
+
+            List<Integer> list = new ArrayList<>();
+
+            for (int j = 0; j < col; j++) {
+                if ((i & (1 << j)) != 0) {
+                    list.add(j);
+                }
+            }
+
+            // 유일성 검사
+            Set<String> set = new HashSet<>();
+            for (String[] row : relation) {
+                StringBuilder sb = new StringBuilder();
+                for (int index : list) {
+                    sb.append(row[index]);
+                }
+                if (!set.add(sb.toString())) {
+                    continue iter;
+                }
+            }
+
+            // 최소성 검사
+            for (List<Integer> key : candidateKeys) {
+                if (list.containsAll(key)) {
+                    continue iter;
+                }
+            }
+
+            candidateKeys.add(list);
+        }
+
+        return Math.max(candidateKeys.size(), 1);
+    }
+}
