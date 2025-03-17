16dda1752ca95f7b2ac06d4a9fc4364b8e4b7a56
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-03-17 09:37:31 +0900

    [level 2] Title: 과제 진행하기, Time: 9.62 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[][] plans) {
+        PriorityQueue<Task> tasks = new PriorityQueue<>(Comparator.comparingInt(task -> task.start));
+        for (String[] plan : plans) {
+            String name = plan[0];
+            int start = Integer.parseInt(plan[1].substring(0, 2)) * 60 + Integer.parseInt(plan[1].substring(3));
+            int remain = Integer.parseInt(plan[2]);
+
+            tasks.offer(new Task(name, start, remain));
+        }
+
+        Deque<Task> deferredTasks = new ArrayDeque<>();
+
+        Task currentTask = tasks.poll();
+        List<String> result = new ArrayList<>();
+
+        while (!tasks.isEmpty() || !deferredTasks.isEmpty()) {
+            Task nextTask = !tasks.isEmpty() ? tasks.peek() : null;
+
+            if (nextTask == null || currentTask.start + currentTask.remain <= nextTask.start) {
+                result.add(currentTask.name);
+                int end = currentTask.start + currentTask.remain;
+                currentTask = null;
+
+                while (!deferredTasks.isEmpty()) {
+                    Task task = deferredTasks.pop();
+                    if (nextTask != null && end + task.remain > nextTask.start) {
+                        int diff = task.remain - (nextTask.start - end);
+                        deferredTasks.push(new Task(task.name, nextTask.start, diff));
+                        currentTask = tasks.poll();
+                        break;
+                    }
+                    end += task.remain;
+                    result.add(task.name);
+                }
+
+                if (currentTask == null && !tasks.isEmpty()) {
+                    currentTask = tasks.poll();
+                }
+
+            } else {
+                int diff = nextTask.start - currentTask.start;
+                deferredTasks.push(new Task(currentTask.name, nextTask.start, currentTask.remain - diff));
+                currentTask = tasks.poll();
+            }
+        }
+
+        if (currentTask != null) {
+            result.add(currentTask.name);
+        }
+
+        return result.toArray(String[]::new);
+    }
+
+    static class Task {
+        String name;
+        int start, remain;
+
+        public Task(String name, int start, int remain) {
+            this.name = name;
+            this.start = start;
+            this.remain = remain;
+        }
+    }
+}
