1ac23019a94167a6398bfd963ee75874c660c228
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-03-17 14:38:45 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -3,13 +3,15 @@ SELECT
     B.FISH_NAME,
     A.LENGTH
 FROM
-    FISH_INFO A
-        JOIN FISH_NAME_INFO B
+    FISH_INFO AS A
+        JOIN FISH_NAME_INFO AS B
             ON A.FISH_TYPE = B.FISH_TYPE
 WHERE
-    (A.FISH_TYPE, A.LENGTH) IN (
+    (A.FISH_TYPE, A.LENGTH) IN 
+            (
              SELECT FISH_TYPE, MAX(LENGTH)
              FROM FISH_INFO
-             GROUP BY FISH_TYPE)
+             GROUP BY FISH_TYPE
+            )
 ORDER BY
-    A.ID
+    1
