02047fdce88c0a9cfb7e062f3ea26209758caa8d
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-04-23 09:00:58 +0900

    [Gold II] Title: 대표 선수, Time: 1088 ms, Memory: 121444 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.java
+++ 백준/Gold/2461. 대표 선수/대표 선수.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,40 +14,48 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n * m][2];
+        int[][] arr = new int[n][m];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-
             for (int j = 0; j < m; j++) {
-                arr[i * m + j][0] = i;
-                arr[i * m + j][1] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
+            Arrays.sort(arr[i]);
         }
 
-        Arrays.sort(arr, Comparator.comparingInt(a -> a[1]));
+        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(node -> node.value));
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            pq.offer(new Node(arr[i][0], i, 0));
+            max = Math.max(max, arr[i][0]);
+        }
 
-        int[] count = new int[n];
-        int classes = 0;
         int ans = Integer.MAX_VALUE;
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
 
-        for (int left = 0, right = 0; right < n * m; right++) {
-            int index = arr[right][0];
-            count[index]++;
+            int value = node.value;
+            int num = node.num;
+            int index = node.index;
+            ans = Math.min(ans, max - value);
 
-            if (count[index] == 1) classes++;
+            if (index + 1 == m) break;
 
-            while (classes == n) {
-                int diff = arr[right][1] - arr[left][1];
-                ans = Math.min(ans, diff);
-
-                count[arr[left][0]]--;
-                if (count[arr[left][0]] == 0) classes--;
-
-                left++;
-            }
+            max = Math.max(max, arr[num][index + 1]);
+            pq.offer(new Node(arr[num][index + 1], num, index + 1));
         }
 
         System.out.println(ans);
+    }
+
+    static class Node {
+        int value, num, index;
+
+        public Node(int value, int num, int index) {
+            this.value = value;
+            this.num = num;
+            this.index = index;
+        }
     }
 }
