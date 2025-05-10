7c932dddb0a3bf742efad13a4b99cd00ca6760fe
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-05-11 08:42:34 +0900

    [Gold IV] Title: 도시 건설, Time: 1012 ms, Memory: 166720 KB -BaekjoonHub

---
+++ 백준/Gold/21924. 도시 건설/도시 건설.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[2]));
+
+        long total = 0;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            pq.offer(new int[]{a, b, c});
+            total += c;
+        }
+
+        int connected = 0;
+
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int a = cur[0];
+            int b = cur[1];
+            int c = cur[2];
+
+            if (find(a) != find(b)) {
+                union(a, b);
+                total -= c;
+                connected++;
+            }
+        }
+
+        System.out.println(connected == n - 1 ? total : -1);
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+}
