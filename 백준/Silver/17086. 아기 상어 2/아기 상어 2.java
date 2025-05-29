598ac7bee13a31bf510d68a1f8241afa84c04da0
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-05-30 08:26:56 +0900

    [Silver II] Title: 아기 상어 2, Time: 368 ms, Memory: 57392 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -8,6 +8,7 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int[][] map;
+    static int[][] dist;
     static int n, m;
     static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
@@ -21,53 +22,58 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
+        dist = new int[n][m];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                dist[i][j] = 101;
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (map[i][j] == 0) {
-                    ans = Math.max(ans, bfs(i, j));
+                if (map[i][j] == 1) {
+                    bfs(i, j);
                 }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans = Math.max(ans, dist[i][j]);
             }
         }
 
         System.out.println(ans);
     }
 
-    private static int bfs(int i, int j) {
+    private static void bfs(int i, int j) {
+        dist[i][j] = 0;
         boolean[][] visit = new boolean[n][m];
         visit[i][j] = true;
 
         Queue<int[]> qu = new ArrayDeque<>();
-        qu.offer(new int[]{i, j, 0});
+        qu.offer(new int[]{i, j});
 
         while (!qu.isEmpty()) {
             int[] now = qu.poll();
             int x = now[0];
             int y = now[1];
-            int count = now[2];
-
-            if (map[x][y] == 1) {
-                return count;
-            }
 
             for (int d = 0; d < 8; d++) {
                 int nx = x + dx[d];
                 int ny = y + dy[d];
 
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m || visit[nx][ny]) continue;
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                if (visit[nx][ny] || map[nx][ny] == 1) continue;
 
                 visit[nx][ny] = true;
-                qu.offer(new int[]{nx, ny, count + 1});
+                dist[nx][ny] = Math.min(dist[nx][ny], dist[x][y] + 1);
+                qu.offer(new int[]{nx, ny});
             }
         }
-
-        return 0;
     }
 }
