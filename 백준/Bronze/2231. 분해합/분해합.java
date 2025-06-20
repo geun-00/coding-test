830f6eee630ff72d6ed278570d7a61ef76c525b2
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-06-21 08:43:49 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(solve(n));
+    }
+
+    private static int solve(int n) {
+        for (int i = 0; i <= n; i++) {
+            int sum = i;
+            int temp = i;
+
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            if (sum == n) {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
