2cf6d53f38b5aae05af5f79f4a94cd6a7d5ab837
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-06-23 08:09:31 +0900

    [Silver V] Title: K번째 수, Time: 1064 ms, Memory: 368396 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,10 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -12,13 +13,67 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
+        arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        quickSort(0, n - 1, k - 1);
         System.out.println(arr[k - 1]);
+    }
+
+    private static void quickSort(int start, int end, int k) {
+        if (start < end) {
+            int pivot = partition(start, end);
+
+            if (pivot < k) {
+                quickSort(pivot + 1, end, k);
+            } else if (k < pivot) {
+                quickSort(start, pivot - 1, k);
+            }
+        }
+    }
+
+    private static int partition(int start, int end) {
+        if (start + 1 == end) {
+            if (arr[start] > arr[end]) {
+                swap(start, end);
+            }
+            return end;
+        }
+        
+        int mid = (start + end) / 2;
+        int pivot = arr[mid];
+        swap(start, mid);
+
+        int left = start + 1;
+        int right = end;
+
+        while (left <= right) {
+            while (right > 0 && arr[right] > pivot) {
+                right--;
+            }
+            while (left < arr.length - 1 && arr[left] < pivot) {
+                left++;
+            }
+
+            if (left <= right) {
+                swap(left, right);
+                left++;
+                right--;
+            }
+        }
+
+        arr[start] = arr[right];
+        arr[right] = pivot;
+
+        return right;
+    }
+
+    private static void swap(int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
