7e0ec172f043cfc9477896e6a40b54a493d80bbd
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-06-22 09:00:58 +0900

    [Silver I] Title: 숨바꼭질, Time: 440 ms, Memory: 33272 KB -BaekjoonHub

---
+++ 백준/Silver/6118. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] graph = new List[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        int[] dist = new int[n + 1];
+        Queue<Integer> qu = new ArrayDeque<>();
+        boolean[] visit = new boolean[n + 1];
+
+        qu.offer(1);
+        visit[1] = true;
+
+        while (!qu.isEmpty()) {
+            int now = qu.poll();
+
+            for (int next : graph[now]) {
+                if (!visit[next]) {
+                    visit[next] = true;
+                    dist[next] = dist[now] + 1;
+                    qu.offer(next);
+                }
+            }
+        }
+
+        int max = Arrays.stream(dist).max().orElse(0);
+        int num = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] == max) {
+                num = i;
+                break;
+            }
+        }
+
+        long count = Arrays.stream(dist).filter(i -> i == max).count();
+
+        System.out.println(num + " " + max + " " + count);
+    }
+}
