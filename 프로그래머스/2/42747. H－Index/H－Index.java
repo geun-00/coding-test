f532d0db1a2f374e36baddefa5e635bec47cc7d5
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-07-04 22:15:21 +0900

    [level 2] Title: H-Index, Time: 0.90 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,15 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
         
-        for(int h = citations.length; h >= 1; h--) {
+        Arrays.sort(citations);
+        
+        int n = citations.length;
+        
+        for(int h = n; h >= 1; h--) {
             
-            int count = 0;
+            int idx = n - h;
             
-            for(int c : citations) {
-                if(c >= h) count++;
-            }
-            
-            if(count >= h) return h;
+            if(citations[idx] >= h) return h;
         }
         
         return 0;
