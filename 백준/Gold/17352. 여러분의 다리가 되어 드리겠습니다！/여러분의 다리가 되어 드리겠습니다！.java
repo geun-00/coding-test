f53673df671611c4752681dcc172a96635571a55
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-07-07 20:45:31 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 540 ms, Memory: 89856 KB -BaekjoonHub

---
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < n - 2; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (a > b) {
+                int temp = a;
+                a = b;
+                b = temp;
+            }
+
+            union(a, b);
+        }
+
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 1; i <= n; i++) {
+            set.add(find(i));
+        }
+
+        Iterator<Integer> iter = set.iterator();
+        System.out.println(iter.next() + " " + iter.next());
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+}
