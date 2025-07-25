ececa778ea8110a56aec338cd0c4ff0eaea2df8b
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-07-25 14:52:02 +0900

    [level 3] Title: 외벽 점검, Time: 121.86 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.java
@@ -0,0 +1,59 @@
+class Solution {
+
+    public int solution(int n, int[] weak, int[] dist) {
+        int[] arr = new int[weak.length];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = weak[i + 1] - weak[i];
+        }
+        arr[arr.length - 1] = weak[0] + n - weak[weak.length - 1];
+
+        boolean[] used = new boolean[dist.length];
+
+        for (int i = 1; i <= dist.length; i++) {
+            if (solve(i, 0, used, dist, new int[i], weak.length, arr)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean solve(int limit, int depth, boolean[] used, int[] dist, int[] friends, int length, int[] arr) {
+        if (limit == depth) {
+            return check(friends, length, arr);
+        }
+
+        for (int i = 0; i < dist.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                friends[depth] = dist[i];
+                if (solve(limit, depth + 1, used, dist, friends, length, arr)) {
+                    return true;
+                }
+
+                used[i] = false;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean check(int[] friends, int length, int[] arr) {
+        for (int i = 0; i < length; i++) {
+            int covered = 0;
+            int start = i;
+
+            for (int f : friends) {
+                while (f - arr[start %= length] >= 0) {
+                    f -= arr[start++];
+                    covered++;
+                }
+                start++;
+                covered++;
+
+                if (covered >= length) return true;
+            }
+        }
+
+        return false;
+    }
+}
