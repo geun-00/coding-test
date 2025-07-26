244ec895679fb63e9a6e2d80cd8a365013028740
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-07-26 11:39:45 +0900

    [Silver I] Title: 현수막, Time: 236 ms, Memory: 26124 KB -BaekjoonHub

---
+++ 백준/Silver/14716. 현수막/현수막.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int n, m;
+    static int[][] arr;
+    static boolean[][] visit;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int ans = 0;
+        visit = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (arr[i][j] == 1 && !visit[i][j]) {
+                    dfs(i, j);
+                    ans++;
+                }
+            }
+        }
+
+        System.out.println(ans);
+    }
+
+    private static void dfs(int x, int y) {
+        visit[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m || arr[nx][ny] == 0 || visit[nx][ny]) {
+                continue;
+            }
+
+            dfs(nx, ny);
+        }
+    }
+}
