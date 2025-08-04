37b71f557439c17dc64a999c6f1cac60c449bf7d
GeunYeong <138780449+geun-00@users.noreply.github.com>
2025-08-04 14:28:48 +0900

    [Gold III] Title: 개구리 점프, Time: 100 ms, Memory: 14068 KB, Score: 19 point -BaekjoonHub

---
+++ 백준/Gold/17619. 개구리 점프/개구리 점프.java
@@ -0,0 +1,100 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        init(n);
+
+        Tree[] trees = new Tree[n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            trees[i] = new Tree(i, x1, x2, y);
+        }
+
+        Arrays.sort(trees, (a, b) -> {
+            return Integer.compare(a.x1, b.x1);
+        });
+
+        for (int i = 0; i < n - 1; i++) {
+            Tree a = trees[i];
+            Tree b = trees[i + 1];
+
+            if (a.x2 >= b.x1) {
+                int u = a.number;
+                int v = b.number;
+
+                if (u > v) {
+                    int temp = u;
+                    u = v;
+                    v = temp;
+                }
+
+                union(u, v);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+
+            sb.append((find(a) == find(b)) ? 1 : 0).append("\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    private static void init(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    static class Tree {
+        int number;
+        int x1, x2, y;
+
+        public Tree(int number, int x1, int x2, int y) {
+            this.number = number;
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y = y;
+        }
+    }
+}
