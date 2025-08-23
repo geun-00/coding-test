bf98a576a09a4af1124f9957c5e740a63b931901
GeunYeong <jgy091411@gmail.com>
2025-08-24 08:17:57 +0900

    [Bronze II] Title: 나는 친구가 적다 (Large), Time: 288 ms, Memory: 23336 KB -BaekjoonHub

---
+++ 백준/Bronze/16172. 나는 친구가 적다 （Large）/나는 친구가 적다 （Large）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        s = s.replaceAll("[0-9]", "");
+        String p = br.readLine();
+
+        System.out.println(KMP(s, p));
+    }
+
+    private static int KMP(String parent, String pattern) {
+        int[] table = makeTable(pattern);
+        int n = parent.length();
+        int m = pattern.length();
+        int j = 0;
+
+        for (int i = 0; i < n; i++) {
+            while (j > 0 && parent.charAt(i) != pattern.charAt(j)) {
+                j = table[j - 1];
+            }
+
+            if (parent.charAt(i) == pattern.charAt(j)) {
+                if (j == m - 1) {
+                    return 1;
+                }
+
+                j++;
+            }
+        }
+
+        return 0;
+    }
+
+    static int[] makeTable(String pattern) {
+        int length = pattern.length();
+        int[] table = new int[length];
+
+        int j = 0;
+        for (int i = 1; i < length; i++) {
+            while (j > 0 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = table[j - 1];
+            }
+
+            if (pattern.charAt(i) == pattern.charAt(j)) {
+                table[i] = ++j;
+            }
+        }
+
+        return table;
+    }
+}
