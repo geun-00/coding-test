ffc5bcc7f03eba6c4c7acff8df7fd2a9fe0e8a10
GeunYeong <jgy091411@gmail.com>
2025-08-27 09:08:06 +0900

    [Bronze I] Title: 2의 보수, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/24389. 2의 보수/2의 보수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int bit = ~n + 1;
+
+        int temp = n ^ bit;
+
+        System.out.println(Integer.bitCount(temp));
+    }
+}
