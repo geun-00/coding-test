2e8a008e87c678ecb3e717e9a3e5ea5b06e59840
GeunYeong <jgy091411@gmail.com>
2025-08-29 09:29:07 +0900

    [Silver II] Title: 타일링, Time: 128 ms, Memory: 14960 KB -BaekjoonHub

--- 백준/Silver/1793. 타일링/타일링.java
+++ 백준/Silver/1793. 타일링/타일링.java
@@ -8,25 +8,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String line;
 
-        BigInteger[][] dp = new BigInteger[251][3];
+        BigInteger[] dp = new BigInteger[251];
 
-        dp[0][0] = BigInteger.valueOf(0);   //2x2
-        dp[0][1] = BigInteger.valueOf(0);   //2x1
-        dp[0][2] = BigInteger.valueOf(1);   //1x2
+        dp[0] = dp[1] = BigInteger.valueOf(1);
+        dp[2] = BigInteger.valueOf(3);
 
-        dp[1][0] = BigInteger.valueOf(0);   //2x2
-        dp[1][1] = BigInteger.valueOf(0);   //2x1
-        dp[1][2] = BigInteger.valueOf(1);   //1x2
-
-        for (int i = 2; i <= 250; i++) {
-            dp[i][0] = dp[i - 1][2];
-            dp[i][1] = dp[i - 1][2];
-            dp[i][2] = dp[i - 1][0].add(dp[i - 1][1].add(dp[i - 1][2]));
+        for (int i = 3; i <= 250; i++) {
+            dp[i] = dp[i - 1].add(dp[i - 2].multiply(BigInteger.valueOf(2)));
         }
 
         while ((line = br.readLine()) != null && !line.isEmpty()) {
             int n = Integer.parseInt(line);
-            System.out.println(dp[n][0].add(dp[n][1].add(dp[n][2])));
+            System.out.println(dp[n]);
         }
     }
 }
