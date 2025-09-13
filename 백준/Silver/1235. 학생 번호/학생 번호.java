2e21cbb4adfba9fde3657c317efd86ef04699160
GeunYeong <jgy091411@gmail.com>
2025-09-13 12:24:42 +0900

    [Silver IV] Title: 학생 번호, Time: 124 ms, Memory: 16528 KB -BaekjoonHub

---
+++ 백준/Silver/1235. 학생 번호/학생 번호.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] names = new String[n];
+        for (int i = 0; i < n; i++) {
+            names[i] = br.readLine();
+        }
+
+        int len = names[0].length();
+
+        for (int k = 1; k < len; k++) {
+            Set<String> set = new HashSet<>();
+
+            for (String name : names) {
+                set.add(name.substring(len - k, len));
+            }
+
+            if (set.size() == n) {
+                System.out.println(k);
+                return;
+            }
+        }
+        
+        System.out.println(len);
+    }
+}
