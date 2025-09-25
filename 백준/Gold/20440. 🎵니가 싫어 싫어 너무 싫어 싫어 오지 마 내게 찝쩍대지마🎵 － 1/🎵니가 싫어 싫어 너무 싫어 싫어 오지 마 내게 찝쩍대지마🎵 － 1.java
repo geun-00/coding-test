484be7cc7515fadc73df10983bb443f6644107fe
GeunYeong <jgy091411@gmail.com>
2025-09-25 09:30:39 +0900

    [Gold III] Title: 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 - 1, Time: 1400 ms, Memory: 481712 KB -BaekjoonHub

---
+++ 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[n][2];
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+
+            list.add(arr[i][0]);
+            list.add(arr[i][1]);
+        }
+
+        List<Integer> sorted = list.stream().distinct().sorted().collect(Collectors.toList());
+        int m = sorted.size();
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            map.put(sorted.get(i), i);
+        }
+
+        int[] diff = new int[m];
+        for (int i = 0; i < n; i++) {
+            diff[map.get(arr[i][0])]++;
+            diff[map.get(arr[i][1])]--;
+        }
+
+        int cur = 0, max = 0;
+        int start = -1, end = -1;
+
+        for (int i = 0; i < m; i++) {
+            cur += diff[i];
+
+            if (max < cur) {
+                max = cur;
+                start = sorted.get(i);
+                end = -1;
+            } else if (cur < max && end == -1) {
+                end = sorted.get(i);
+            }
+        }
+
+        if (end == -1) {
+            end = sorted.get(m - 1);
+        }
+
+        System.out.println(max);
+        System.out.println(start + " " + end);
+    }
+}
