5346167843cb8864054138dd24126a9003f7b9e5
GeunYeong <jgy091411@gmail.com>
2025-10-12 09:30:04 +0900

    [Silver V] Title: 짐 챙기는 숌, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Silver/1817. 짐 챙기는 숌/짐 챙기는 숌.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+        int sum = 0;
+
+        if (n > 0) {
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                int w = Integer.parseInt(st.nextToken());
+
+                sum += w;
+                if (sum > m) {
+                    sum = w;
+                    ans++;
+                }
+            }
+
+            if (sum > 0) {
+                ans++;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
