de2eef47ce26480eb520384322edc9daa9f49208
GeunYeong <jgy091411@gmail.com>
2025-10-26 18:53:05 +0900

    [Gold V] Title: 귀찮은 해강이, Time: 372 ms, Memory: 51328 KB -BaekjoonHub

---
+++ 백준/Gold/24391. 귀찮은 해강이/귀찮은 해강이.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        parent = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            union(a, b);
+        }
+
+        int prev = 0;
+        int ans = 0;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+
+            int r = find(a);
+
+            if (prev != r) {
+                ans++;
+            }
+
+            prev = r;
+        }
+
+        System.out.println(ans - 1);
+    }
+
+    private static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        if (a != b) {
+            if (a < b) {
+                parent[a] = b;
+            } else {
+                parent[b] = a;
+            }
+        }
+    }
+
+    private static int find(int a) {
+        if (parent[a] == a) {
+            return a;
+        }
+        return parent[a] = find(parent[a]);
+    }
+}
