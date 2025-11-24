6c769ffd26879f1ae486a496c46587f862ee1140
GeunYeong <jgy091411@gmail.com>
2025-11-24 20:01:06 +0900

    [Silver I] Title: 팰린드롬 파티션, Time: 96 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Silver/2705. 팰린드롬 파티션/팰린드롬 파티션.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[1001];
+        dp[1] = 1;
+
+        for (int i = 2; i <= 1000; i++) {
+            if (i % 2 == 0) {
+                dp[i] = dp[i - 1] + dp[i / 2];
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        }
+
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
+    }
+}
