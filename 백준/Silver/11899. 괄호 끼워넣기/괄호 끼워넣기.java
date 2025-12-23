59afb7174d3294e6d9f9c5ace8cbc52cdd1ceb2f
GeunYeong <jgy091411@gmail.com>
2025-12-23 21:14:21 +0900

    [Silver III] Title: 괄호 끼워넣기, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/11899. 괄호 끼워넣기/괄호 끼워넣기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] s = br.readLine().toCharArray();
+
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char ch : s) {
+            if (ch == ')') {
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    stack.push(ch);
+                }
+            } else {
+                stack.push(ch);
+            }
+        }
+
+        System.out.println(stack.size());
+    }
+}
