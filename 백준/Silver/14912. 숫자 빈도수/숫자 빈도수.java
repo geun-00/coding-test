bd62731815e217c61958e5daac853a7d8d97fe92
GeunYeong <jgy091411@gmail.com>
2025-12-28 20:32:59 +0900

    [Silver V] Title: 숫자 빈도수, Time: 116 ms, Memory: 14780 KB -BaekjoonHub

---
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            int num = i;
+
+            while (num > 0) {
+                if (num % 10 == d) {
+                    ans++;
+                }
+                num /= 10;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
