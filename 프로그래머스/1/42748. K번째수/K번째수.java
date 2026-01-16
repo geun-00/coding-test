f24faef1f13cc98074e9db37a20fef605bf43313
geun-00 <jgy091411@gmail.com>
2026-01-16 16:25:57 +0900

    [level 1] Title: K번째수, Time: 0.52 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0; i < commands.length; i++) {
+            
+            int from = commands[i][0] - 1;
+            int to = commands[i][1];
+            int k = commands[i][2] - 1;
+            
+            int[] temp = Arrays.copyOfRange(array, from, to);
+            Arrays.sort(temp);
+            
+            answer[i] = temp[k];
+        }
+        
+        return answer;
+    }
+}
