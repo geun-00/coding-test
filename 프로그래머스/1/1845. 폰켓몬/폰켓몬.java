1f88aa43c3a6d5e1a112b9234930313bb25adfda
geun-00 <jgy091411@gmail.com>
2026-01-25 21:55:00 +0900

    [level 1] Title: 폰켓몬, Time: 4.46 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,10 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        
-        HashSet<Integer> set = new HashSet<>();
-        
-        for(int num : nums) set.add(num);
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums ) set.add(n);
         
         return Math.min(nums.length / 2, set.size());
     }
