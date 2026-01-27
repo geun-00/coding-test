89499c695d96934b0f43f62421a9ad0bf433f747
geun-00 <jgy091411@gmail.com>
2026-01-27 22:36:30 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.92 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -2,34 +2,53 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] jobs) {
+        PriorityQueue<Job> waitQu = new PriorityQueue<>((a, b) -> {
+            if (a.time != b.time) return a.time - b.time;
+            else if (a.start != b.start) return a.start - b.start;
+            return a.id - b.id;
+        });
         
-        Arrays.sort(jobs, Comparator.comparingInt(a -> a[0]));
-
-        PriorityQueue<int[]> jobQu = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-
-        int jobNums = jobs.length;
-
-        int curTime = 0;
-        int jobIdx = 0;
-        int total = 0;
-        int process = 0;
-
-        while (jobIdx < jobNums) {
-
-            while (jobIdx < jobNums && jobs[jobIdx][0] <= curTime) {
-                jobQu.offer(jobs[jobIdx++]);
-            }
-
-            if (!jobQu.isEmpty()) {
-                int[] job = jobQu.poll();
-                total += job[1] + curTime - job[0];
-                curTime += job[1];
-                process++;
-            } else {
-                curTime=jobs[jobIdx][0];
-            }
+        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        
+        int n = jobs.length;
+        Job[] tasks = new Job[n];
+        
+        for (int i = 0; i < n; i++) {
+            tasks[i] = new Job(jobs[i][1], jobs[i][0], i);
         }
-
-        return total / jobNums;
+        
+        int index = 0;
+        int total = 0;
+        int prev = 0;
+        
+        while (index < n || !waitQu.isEmpty()) {
+            
+            while (index < n && prev >= jobs[index][0]) {
+                waitQu.offer(tasks[index]);
+                index++;
+            }
+            
+            if (waitQu.isEmpty()) {
+                prev = jobs[index][0];
+                continue;
+            }
+            
+            Job job = waitQu.poll();
+            
+            total += job.time + prev - job.start;        
+            prev += job.time;
+        }
+        
+        return total / n;
+    }
+    
+    static class Job {
+        int time, start, id;
+        
+        public Job(int t, int s, int i) {
+            time = t;
+            start = s;
+            id = i;
+        }
     }
 }
