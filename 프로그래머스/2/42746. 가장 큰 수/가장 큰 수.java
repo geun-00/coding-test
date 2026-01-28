8b5abe587934c1e90faab9c4c33c97df4c65dafa
geun-00 <jgy091411@gmail.com>
2026-01-28 22:08:06 +0900

    [level 2] Title: 가장 큰 수, Time: 202.02 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,6 +1,25 @@
+import java.util.*;
+
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        return answer;
+        int n = numbers.length;
+        String[] arr = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = numbers[i] + "";
+        }
+        
+        Arrays.sort(arr, (a, b) -> {
+            return (a+b).compareTo(b+a);
+        });
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = n - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+        
+        String ans = sb.toString();
+        
+        return ans.startsWith("0") ? "0" : ans;
     }
 }
