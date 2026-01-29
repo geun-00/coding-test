c7e8d5c635ed3d40ef9c9e06c742099fc31a2e71
geun-00 <jgy091411@gmail.com>
2026-01-29 19:33:28 +0900

    [level 1] Title: 모의고사, Time: 4.16 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,44 +1,35 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
-        
         int[][] arr = {
-            {1, 2, 3, 4, 5},
-            {2, 1, 2, 3, 2, 4, 2, 5},
-            {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
         };
         
-        int[] temp = new int[3];
+        List<Integer> list = new ArrayList<>();
         int max = 0;
         
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
+
+            int count = 0;
             
-            for(int j = 0; j < answers.length; j++) {
-                
-                if(arr[i][j % arr[i].length] == answers[j]) {
-                    temp[i]++;
-                    
-                    max = Math.max(max, temp[i]);
+            for (int j = 0; j < answers.length; j++) {
+                if (arr[i][j  % arr[i].length] == answers[j]) {
+                    count++;
                 }
             }
-        }
-        
-        int count = 0;
-        
-        for(int i : temp) {
-            if(i == max) {
-                count++;
+            
+            if (max < count) {
+                max = count;
+                list.clear();
+                list.add(i + 1);
+            } else if (max == count) {
+                list.add(i + 1);
             }
         }
         
-        int[] ans = new int[count];
-        int idx = 0;
-        
-        for(int i = 0; i < 3; i++) {
-            if(temp[i] == max) {
-                ans[idx++] = i + 1;
-            }
-        }
-        
-        return ans;
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
