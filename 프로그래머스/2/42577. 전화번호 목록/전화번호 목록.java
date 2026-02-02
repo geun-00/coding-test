d82656faaa29c8e162c686a6fc63f5b792e91ce3
geun-00 <jgy091411@gmail.com>
2026-02-02 21:21:36 +0900

    [level 2] Title: 전화번호 목록, Time: 363.04 ms, Memory: 98.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,16 +1,16 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-    
         Arrays.sort(phone_book);
-
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
+        int len = phone_book.length;
+        
+        for (int i = 0; i < len - 1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
                 return false;
             }
         }
-
+        
         return true;
     }
 }
